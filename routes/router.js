ded201246d790f9f7b640c790ec0d58459ba89d3
gangeshberi <gangeshberi1@gmail.com>
2019-06-18 19:23:54 -0400

    api changes and backend routing

--- routes/router.js
+++ routes/router.js
@@ -7,11 +7,16 @@ const responseEvaluation = require('./api/response_evaluation_api');
 const mutation = require('./api/sequencing_data_api');
 
 
+// APIs related to the drug screening table.
 router.get('/v1/drug', drugScreening.getDrug);
+router.get('/v1/treatment', drugScreening.getDrugScreening);
+router.get('/v1/untreated', drugScreening.getUntreated);
 
+// APIs related to the response evaluation table.
 router.get('/v1/respeval', responseEvaluation.getResponseEvaluation);
 
-router.get('/v1/mutation/:id', mutation.isValidId, mutation.getMutationId)
-router.get('/v1/mutation', mutation.getMutation)
+// APIs related to the mutation table.
+router.get('/v1/mutation/:id', mutation.isValidId, mutation.getMutationId);
+router.get('/v1/mutation', mutation.getMutation);
 
 module.exports = router;
