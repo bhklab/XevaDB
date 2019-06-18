60f2c10a86fcf9b1ead20ad21d43858b79ae0c9f
gangeshberi <gangeshberi1@gmail.com>
2019-06-18 19:22:30 -0400

    react-router and query strings for heatmap

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { HeatMap, Oncoprint } from './Components/index';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-  return (
-     <div>
-        <HeatMap/>
-        <Oncoprint/>
-     </div>
-  )
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HeatMap}></Route>
+          <Route path="/" exact component={Oncoprint}></Route>
+        </Switch>
+      </Router>
+    )
   }
 }
 
