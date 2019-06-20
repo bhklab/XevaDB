7cba73e3bafd6694ab9243d6af6041bf5d524d73
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-06-20 15:49:35 -0400

    added code to specify experiment type

--- client/src/Components/TumorGrowthCurve/TumorGrowthCurve.js
+++ client/src/Components/TumorGrowthCurve/TumorGrowthCurve.js
@@ -22,6 +22,7 @@ class TumorGrowthCurve extends React.Component {
         const params = new URLSearchParams(this.props.location.search)
         let patient_param = params.get('patient');
         let drugid_param = params.get('drug_id');
+
         return {patient_param: patient_param, drugid_param: drugid_param}
     }
 
@@ -33,67 +34,71 @@ class TumorGrowthCurve extends React.Component {
     // filters by batch - comment out the batch lines for full data
     // call this command to create a curve!!!
     dataParse(data) {
-        console.log(data)
         let data_formatted = []
         let batch_select = data[1]["patient_id"]
-        console.log(batch_select)
         let batches = []
-            
-            for (let i = 0; i < data.length; i++) {
-                batches.push(data[i]["patient_id"])
-                if (data[i]["patient_id"] === batch_select) {
-                    if (data[i]["time"] === 0) {
-                        var new_datapt = {
-                            exp_type: data[i]["exp.type"],
+
+        // loop throuth the data and set the values in the variable.
+        for (let i = 0; i < data.length; i++) {
+            batches.push(data[i]["patient_id"])
+            if (data[i]["patient_id"] === batch_select) {
+                if (data[i]["time"] === 0) {
+                    let exp_type = ""
+                    //this is used to set the value of exp_type.
+                    if(data[i]["drug"] === "untreated") { 
+                        exp_type = "control" 
+                    } 
+                    else { 
+                        exp_type = "treatment" 
+                    }
+                    var new_datapt = {
+                        exp_type: exp_type,
+                        batch: data[i]["patient_id"],
+                        model: data[i]["model_id"],
+                        drug: data[i]["drug"],
+                        pdx_points: [{
+                            times: [parseInt(data[i]["time"])],
+                            volumes: [parseInt(data[i]["volume"])]
+                        }],
+                        pdx_json: [{
+                            model: data[i]["model_id"].replace(/\./g,' ').replaceAll(" ", "-"),
                             batch: data[i]["patient_id"],
-                            model: data[i]["model_id"],
-                            drug: data[i]["drug"],
-                            pdx_points: [{
-                                times: [parseInt(data[i]["time"])],
-                                volumes: [parseInt(data[i]["volume"])]
-                            }],
-                            pdx_json: [{
-                                model: data[i]["model_id"].replace(/\./g,' ').replaceAll(" ", "-"),
+                            exp_type: exp_type,
+                            time: parseInt(data[i]["time"]),
+                            volume: parseInt(data[i]["volume"])
+                        }]
+                        
+                    }
+                    data_formatted.push(new_datapt)
+                } 
+                else {
+                    if (data[i]["time"] <= 70) {
+                        data_formatted[data_formatted.length - 1].pdx_points[0].times.push(parseInt(data[i]["time"]))
+                        data_formatted[data_formatted.length - 1].pdx_points[0].volumes.push(parseInt(data[i]["volume"]))
+                        data_formatted[data_formatted.length - 1].pdx_json.push(
+                            {
+                                model: data[i]["model_id"],
                                 batch: data[i]["patient_id"],
                                 exp_type: data[i]["exp.type"],
                                 time: parseInt(data[i]["time"]),
                                 volume: parseInt(data[i]["volume"])
-                            }]
-                            
-                        }
-                        data_formatted.push(new_datapt)
-                    } 
-                    else {
-                        if (data[i]["time"] <= 70) {
-                            data_formatted[data_formatted.length - 1].pdx_points[0].times.push(parseInt(data[i]["time"]))
-                            data_formatted[data_formatted.length - 1].pdx_points[0].volumes.push(parseInt(data[i]["volume"]))
-                            data_formatted[data_formatted.length - 1].pdx_json.push(
-                                {
-                                    model: data[i]["model_id"],
-                                    batch: data[i]["patient_id"],
-                                    exp_type: data[i]["exp.type"],
-                                    time: parseInt(data[i]["time"]),
-                                    volume: parseInt(data[i]["volume"])
-                                }
-                            )
-                        }
+                            }
+                        )
                     }
                 }
             }
+        }
 
-            //normalizing
-            for (var i = 0; i < data_formatted.length; i++) {
-                var item = data_formatted[i]
-                var first = item.pdx_points[0].volumes[0]
-                for (var j = 0; j < item.pdx_points[0].volumes.length; j++) {
-                    data_formatted[i].pdx_points[0].volumes[j] = this.norm(item.pdx_points[0].volumes[j], first)
-                    data_formatted[i].pdx_json[j].volume = item.pdx_points[0].volumes[j]
-                }
+        //normalizing
+        for (var i = 0; i < data_formatted.length; i++) {
+            var item = data_formatted[i]
+            var first = item.pdx_points[0].volumes[0]
+            for (var j = 0; j < item.pdx_points[0].volumes.length; j++) {
+                data_formatted[i].pdx_points[0].volumes[j] = this.norm(item.pdx_points[0].volumes[j], first)
+                data_formatted[i].pdx_json[j].volume = item.pdx_points[0].volumes[j]
             }
-            console.log(data_formatted)
-            this.setState({ data: data_formatted})
-           // tumourCurve(data_formatted, "plot")
-    
+        }
+        this.setState({ data: data_formatted})
     }
 
     componentDidMount() {
@@ -110,11 +115,10 @@ class TumorGrowthCurve extends React.Component {
         this.TumorGrowthCurve()
     }
 
-    // Grab the states and pass it to the main function.
+    // Grab the states, node access and pass it to the main function.
     TumorGrowthCurve() {
         const node = this.node;
         let data = this.state.data
-        console.log(data)
         this.makeTumorGrowthCurve(this.state.data, node)
     }
 
@@ -122,8 +126,6 @@ class TumorGrowthCurve extends React.Component {
 
     makeTumorGrowthCurve(node) {
         this.node = node
-        console.log(this.state.data)
-        
         String.prototype.replaceAll = String.prototype.replaceAll || function(s, r) {
             return this.replace(new RegExp(s, 'g'), r);
         }
