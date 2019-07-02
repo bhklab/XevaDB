287db7929da14cb98664e4c8eab489b14e474df7
gangeshberi <gangeshberi1@gmail.com>
2019-07-02 14:40:58 -0400

    testing

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : '',
+      password : 'beri@2493',
       database : 'XevaDB'
     },
     migrations: {
