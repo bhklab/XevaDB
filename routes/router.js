33e11adf5bbf222969f0742642338aca56df415b
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-08 12:18:20 -0400

    adding new function to the API

--- routes/router.js
+++ routes/router.js
@@ -5,18 +5,27 @@ const router = express.Router();
 const drugScreening = require('./api/drug_screening_api');
 const responseEvaluation = require('./api/response_evaluation_api');
 const mutation = require('./api/sequencing_data_api');
+const modelInformation = require('./api/model_information_api');
 
 
 // APIs related to the drug screening table.
-router.get('/v1/drug', drugScreening.getDrug);
 router.get('/v1/treatment', drugScreening.getDrugScreening);
 router.get('/v1/untreated', drugScreening.getUntreated);
+
 
 // APIs related to the response evaluation table.
 router.get('/v1/respeval', responseEvaluation.getResponseEvaluation);
 
+
 // APIs related to the mutation table.
 router.get('/v1/mutation/:id', mutation.isValidId, mutation.getMutationId);
 router.get('/v1/mutation', mutation.getMutation);
+
+
+// APIs related to models.
+router.get('/v1/drugs', modelInformation.getDistinctDrugs);
+router.get('/v1/tissues', modelInformation.getDistinctTissues);
+
+
 
 module.exports = router;
