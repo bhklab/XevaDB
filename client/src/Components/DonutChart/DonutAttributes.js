9fb97e05d0e66c5453f5075fa0ce183fbf4dab38
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-08 12:06:19 -0400

    initial donut chart

---
+++ client/src/Components/DonutChart/DonutAttributes.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import DonutChart from './DonutChart';
+import axios from 'axios';
+
+class AttributesDonut extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            data : []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:5000/api/v1/tissues`)
+             .then((response) => {
+                 this.setState({
+                     data : response.data
+                 })
+             })
+    }
+
+    dimensions = {
+        width: 1000,
+        height: 1500
+    }
+
+    margin = {
+        top: 300,
+        right: 200,
+        bottom: 100,
+        left: 350
+    }
+
+    chartId = 'donut'
+
+    render() {
+        return (
+            <div className='AttributesDonut'>
+                <DonutChart 
+                    dimensions={this.dimensions} margin={this.margin} 
+                    chartId={this.chartId} data={this.state.data}
+                />
+            </div>
+        )
+    }
+}
+
+export default AttributesDonut
