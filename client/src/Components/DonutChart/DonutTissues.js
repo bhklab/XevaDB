cf6de31ebf530d718992e8f937316f2bad7d5e0c
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-11 17:22:36 -0400

    components for drug and tissue donut

--- client/src/Components/DonutChart/DonutTissues.js
+++ client/src/Components/DonutChart/DonutTissues.js
@@ -12,10 +12,17 @@ class DonutTissue extends React.Component {
     }
 
     componentDidMount() {
+        let new_values = []
         axios.get(`http://localhost:5000/api/v1/tissues`)
              .then((response) => {
+                 response.data.data.map((data) => {
+                     let value = {}
+                     value['id'] = data.tissue
+                     value['value'] = data.total
+                     new_values.push(value)
+                 })
                  this.setState({
-                     data : response.data
+                     data : new_values
                  })
              })
     }
