80905644bfbe7f9b3f73963e8a5aa25ba1bf2e97
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-12 22:50:43 -0400

    code clearning

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { HeatMap, Oncoprint, TumorGrowthCurve, DonutTissue, DonutDrug } from './Components/index';
+import { HeatMap, Oncoprint, TumorGrowthCurve, DonutTissue, DonutDrug, Header } from './Components/index';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={HeatMap}></Route>
-        <Route path="/" exact component={Oncoprint}></Route>
+        <Route path="/maps" exact component={HeatMap}></Route>
+        <Route path="/maps" exact component={Oncoprint}></Route>
         <Switch>
           <Route path="/curve" exact component={TumorGrowthCurve}></Route>
-          <Route path='/donut' exact component={DonutTissue}></Route>
+          <Route path='/donut_tissue' exact component={DonutTissue}></Route>
           <Route path='/donut_drug' exact component={DonutDrug}></Route>
+          <Route path='/' exact component={Header}></Route>
         </Switch>
       </Router>
     )
