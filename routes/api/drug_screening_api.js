1181c22b9034b1b3d2ce59405685d2224c66b482
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-16 11:33:02 -0400

    new drugscreening functions

--- routes/api/drug_screening_api.js
+++ routes/api/drug_screening_api.js
@@ -4,10 +4,9 @@ const knex = require('../../db/knex1');
 const getDrugScreening = function(req,res) {
     let drug = req.query.drug;
     let patient = req.query.patient;
-
     knex.select()
         .from('drug_screening')
-        .where(() => {
+        .where(function() {
             this.where('drug', drug)
                 .orWhere('drug', 'untreated')
         })
@@ -40,8 +39,77 @@ const getUntreated = function (req,res) {
         }))
 }
 
+// This will give a list of the patients those are 
+// not availabe in the drug_Screening table ie they don't have drug screening data.
+const getNotDrugAvailable = function(req,res) {
+    knex('model_information')
+        .distinct('model_information.patient_id')
+        .leftJoin(
+            knex('drug_screening')
+            .distinct('patient_id').as('drug_screening')
+            , 'model_information.patient_id'
+            , 'drug_screening.patient_id'
+        )
+        .where('drug_screening.patient_id', null)
+        .then((patient) => {
+            res.send(patient)
+        })
+        .catch(error => res.status(500).json({
+            status: 'an error has occured',
+            data: error
+        }))
+}
+
+
+// This will give a list of the patients those are 
+// not availabe in the sequencing_data table ie they only have drug screening data.
+const getOnlyDrugData = function(req,res) {
+    knex('drug_screening')
+        .distinct('drug_screening.patient_id')
+        .leftJoin(
+            knex('sequencing_data')
+            .distinct('patient_id').as('sequencing_data')
+            , 'drug_screening.patient_id'
+            , 'sequencing_data.patient_id'
+        )
+        .where('sequencing_data.patient_id', null)
+        .then((patients) => {
+            res.send(patients)
+        })
+        .catch(error => res.status(500).json({
+            status: 'an error has occured',
+            data: error
+        }))
+}
+
+
+// This will give a list of the patients those are 
+// not availabe in the sequencing_data table ie they only have drug screening data.
+const getInBoth = function(req,res) {
+    knex('drug_screening')
+        .distinct('drug_screening.patient_id')
+        .leftJoin(
+            knex('sequencing_data')
+            .distinct('patient_id').as('sequencing_data')
+            , 'drug_screening.patient_id'
+            , 'sequencing_data.patient_id'
+        )
+        .whereNotNull('sequencing_data.patient_id')
+        .then((patients) => {
+            res.send(patients)
+        })
+        .catch(error => res.status(500).json({
+            status: 'an error has occured',
+            data: error
+        }))
+}
+
+
 
 module.exports = {
     getDrugScreening,
-    getUntreated
+    getUntreated,
+    getNotDrugAvailable,
+    getOnlyDrugData,
+    getInBoth
 }
