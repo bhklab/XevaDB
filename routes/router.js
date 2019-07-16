5c410bc42064c9b305b1e75c6ad7d7d140c8ee28
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-16 11:34:01 -0400

    routing changes

--- routes/router.js
+++ routes/router.js
@@ -11,6 +11,9 @@ const modelInformation = require('./api/model_information_api');
 // APIs related to the drug screening table.
 router.get('/v1/treatment', drugScreening.getDrugScreening);
 router.get('/v1/untreated', drugScreening.getUntreated);
+router.get('/v1/drugdatanotavailable', drugScreening.getNotDrugAvailable);
+router.get('/v1/getonlydrug', drugScreening.getOnlyDrugData);
+router.get('/v1/getinboth', drugScreening.getInBoth)
 
 
 // APIs related to the response evaluation table.
@@ -20,11 +23,14 @@ router.get('/v1/respeval', responseEvaluation.getResponseEvaluation);
 // APIs related to the mutation table.
 router.get('/v1/mutation/:id', mutation.isValidId, mutation.getMutationId);
 router.get('/v1/mutation', mutation.getMutation);
+router.get('/v1/patientnottested', mutation.getNotTestedPatient);
+router.get('/v1/getonlysequencing', mutation.getOnlySequenceData);
 
 
 // APIs related to models.
 router.get('/v1/drugs', modelInformation.getDistinctDrugs);
 router.get('/v1/tissues', modelInformation.getDistinctTissues);
+router.get('/v1/patients', modelInformation.getDistinctPatients);
 
 
 
