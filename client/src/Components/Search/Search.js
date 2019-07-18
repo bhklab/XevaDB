d6eb6538a2070d78f8dd57b10993571405fef3be
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-17 20:12:39 -0400

    basic search component

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -9,15 +9,52 @@ const StyleBar = styled.div`
         line-height: 0.4;
         margin-top: 150px;
    } 
+
+   input {
+      width: 50%;
+      padding: 14px;
+      margin: 8px 0;
+      box-sizing: border-box;
+      border: 2px solid #bd0808;
+      border-radius: 4px;
+      outline: none;
+
+        :focus {
+            background-color: #f6eeee;
+        }
+
+        ::placeholder {
+            color:  #bd0808;
+            font-size: 14px;
+        }   
+   }
+
 `
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 0
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        console.log(event.target.value)
+        this.setState({value: event.target.value});
+    }
+
+
     render() {
         return (
             <StyleBar>
             <div>
-                <h1> XevaDB </h1>
-                <h2> A Database for PDX pharmacogenomic data </h2>
+                <h2> XevaDB </h2>
+                <h2> A database for PDX pharmacogenomic data </h2>
+                <form>
+                    <input type="text" onChange={this.handleChange} placeholder="Search..."></input>
+                </form>
             </div>
             </StyleBar>
         )
