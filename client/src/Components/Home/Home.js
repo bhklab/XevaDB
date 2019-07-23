b7aedb0363bb6c150c7e05d2f3e46024dfce2a88
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-23 13:55:57 -0400

    code cleaning

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -5,35 +5,8 @@ import DonutTissue from '../DonutChart/DonutTissues'
 import DonutPatient from '../DonutChart/DonutPatient'
 import Search from '../Search/Search'
 import CounterNav from '../CounterNav/CounterNav'
-import styled from 'styled-components'
+import Container from './HomeStyle'
 
-
-
-const Container = styled.div`
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-  .chart {
-    display: flex;
-    flex: 1;
-    justify-content: space-around;
-    margin-top: 20vh;
-    width: 100%;
-    margin-bottom: 80px;
-  }
-
-    @media screen and (max-width: 1300px) {
-        .chart {
-            justify-content: space-between;
-        }
-    }
-
-    @media screen and (max-height: 950px) {
-        .chart {
-            margin-top: 5vh;
-        }
-    }
-`
 
 class Home extends React.Component {
     render() {
@@ -53,5 +26,6 @@ class Home extends React.Component {
         )
     }
 }
+
 
 export default Home
