25e15ae6f16764d79de38369de7f6ebce61cdbe6
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-22 21:12:38 -0400

    some ui changes

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -18,11 +18,19 @@ const Container = styled.div`
     flex: 1;
     justify-content: space-around;
     margin-top: 20vh;
+    width: 100%;
+    margin-bottom: 80px;
   }
 
-  @media screen and (max-width: 1300px) {
+    @media screen and (max-width: 1300px) {
         .chart {
             justify-content: space-between;
+        }
+    }
+
+    @media screen and (max-height: 950px) {
+        .chart {
+            margin-top: 5vh;
         }
     }
 `
