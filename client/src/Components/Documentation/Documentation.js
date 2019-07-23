55a0d5aa90cca7963e478d352f0fc013ef2c91fe
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-23 12:49:50 -0400

    added dummy documentation component

---
+++ client/src/Components/Documentation/Documentation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Documentation extends React.Component {
+
+    render() {
+        return (
+            <h1> Documentation will be added Later. </h1>
+        )
+    }
+}
+
+
+export default Documentation
