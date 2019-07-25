b8a330f8486617df5c0fe9ffb57776a3a11033be
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-25 17:06:44 -0400

    identation of the search style

--- client/src/Components/Search/SearchStyle.js
+++ client/src/Components/Search/SearchStyle.js
@@ -36,64 +36,64 @@ const StyleBar = styled.div`
 
 const customStyles = {
     placeholder: (provided) => ({
+          ...provided,
+          color: '#bd0808',
+          fontSize: '20px'
+    }),
+
+    control: (provided) => ({
+        ...provided,
+        '&:hover': { borderColor: '#bd0808' },
+        border: '1.5px solid #bd0808',
+        boxShadow: 'none',
+        padding: '15px',
+        borderRadius: '18px'
+    }),
+
+    indicatorSeparator: (provided) => ({
+        ...provided,
+        background: '#bd0808',
+        '&:hover': { background: '#bd0808'},
+        height: '30px'
+    }), 
+    
+    dropdownIndicator: (provided ) => ({
+        ...provided,
+        color: '#bd0808',
+        '&:hover': {
+          color: '#bd0808',
+          cursor: 'pointer'
+        }
+    }),
+
+    option: (provided) => ({
+        ...provided,
+        color: '#bd0808',
+        background: '#ffffff',
+        margin: '5px 0px',
+        '&:hover': {
+          background: '#fee8c8'
+        },
+        fontSize: '18px'
+    }),
+
+    singleValue: (provided) => ({
         ...provided,
         color: '#bd0808',
         fontSize: '20px'
     }),
 
-    control: (provided) => ({
-      ...provided,
-      '&:hover': { borderColor: '#bd0808' },
-      border: '1.5px solid #bd0808',
-      boxShadow: 'none',
-      padding: '15px',
-      borderRadius: '18px'
-    }),
-
-    indicatorSeparator: (provided) => ({
-      ...provided,
-      background: '#bd0808',
-      '&:hover': { background: '#bd0808'},
-      height: '30px'
-    }), 
-    
-    dropdownIndicator: (provided ) => ({
-      ...provided,
-      color: '#bd0808',
-      '&:hover': {
-        color: '#bd0808',
-        cursor: 'pointer'
-      }
-    }),
-
-    option: (provided) => ({
-      ...provided,
-      color: '#bd0808',
-      background: '#ffffff',
-      margin: '5px 0px',
-      '&:hover': {
-        background: '#fee8c8'
-      },
-      fontSize: '18px'
-    }),
-
-    singleValue: (provided) => ({
-      ...provided,
-      color: '#bd0808',
-      fontSize: '20px'
-    }),
-
     input: (provided) => ({
-      ...provided,
-      color: '#bd0808',
+        ...provided,
+        color: '#bd0808',
     }),
 
     clearIndicator: (provided) => ({
-      ...provided,
-      color: '#bd0808',
-      '&:hover': {
-        color: '#fee8c8',
-      }
+        ...provided,
+        color: '#bd0808',
+        '&:hover': {
+          color: '#fee8c8',
+        }
     })
 }
 
