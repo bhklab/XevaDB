313e0e801160acc9859d0aff7c4dab8298d57098
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-07-31 12:10:36 -0400

    structuring both files

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,29 +1,33 @@
-import HeatMapData from './HeatMap/HeatMapData'
-import OncoprintData from './Oncoprint/OncoprintData'
-import TumorGrowthCurve from './TumorGrowthCurve/TumorGrowthCurve'
-import DonutChart from './DonutChart/DonutChart'
-import Drug from './Drug/Drug'
-import Tissue from './Tissue/Tissue'
-import TopNav from './TopNav/TopNav'
-import Home from './Home/Home'
-import DonutPatient from './DonutChart/DonutPatient'
 import CounterNav from './CounterNav/CounterNav'
 import Documentation from './Documentation/Documentation'
+import DonutChart from './DonutChart/DonutChart'
+import DonutPatient from './DonutChart/DonutPatient'
+import Drug from './Drug/Drug'
 import DrugSearch from './Drug/DrugSearch'
 import Footer from './Footer/Footer'
+import HeatMapData from './HeatMap/HeatMapData'
+import Home from './Home/Home'
+import OncoprintData from './Oncoprint/OncoprintData'
+import Tissue from './Tissue/Tissue'
+import TopNav from './TopNav/TopNav'
+import TumorGrowthCurve from './TumorGrowthCurve/TumorGrowthCurve'
+
+
+
+
 
 export {
-    HeatMapData,
-    OncoprintData,
-    TumorGrowthCurve,
-    DonutChart,
-    Tissue,
-    TopNav,
-    Home,
-    DonutPatient,
     CounterNav,
     Documentation,
+    DonutChart,
+    DonutPatient,
+    Drug,
     DrugSearch,
     Footer,
-    Drug
+    HeatMapData,
+    Home,
+    OncoprintData,
+    Tissue,
+    TopNav,
+    TumorGrowthCurve,
 }
