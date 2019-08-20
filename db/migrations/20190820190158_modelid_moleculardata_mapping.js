93020652335ffdd03a57dc907c809e6bd664e9db
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-08-20 19:09:47 -0400

    hopefully final database migrations

---
+++ db/migrations/20190820190158_modelid_moleculardata_mapping.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('modelid_moleculardata_mapping', (table) => {
+        table.increments();
+        table.integer('model_id')
+             .notNullable()
+             .unsigned()
+             .references('model_id')
+             .inTable('models')
+             .index();
+        table.integer('sequencing_uid')
+             .notNullable()
+             .unsigned()
+             .references('sequencing_uid')
+             .inTable('sequencing')
+             .index();
+        table.string('mDataType')
+             .notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('modelid_moleculardata_mapping');
+};
