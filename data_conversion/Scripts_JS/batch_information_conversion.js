b7bbe9bad3a617084c74d289a10c97d791071eca
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-03 18:48:11 -0400

    changes for batch information file

--- data_conversion/Scripts_JS/batch_information_conversion.js
+++ data_conversion/Scripts_JS/batch_information_conversion.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 let MultiStream = require('multistream')
 
 // this will take the file_reader function from file_reader file used to loop through the files.
+const file_iterator = require('./file_iterator')
 const file_reader = require('./file_reader')
 
 //folder from where the files will be read.
@@ -17,15 +18,24 @@ const results = [];
 let streams = [];
 
 
-//creating a writable stream which writes to growthcurve_data csv file.
-var csvStream = csv.createWriteStream({headers: ["batch", "model_id", "type"]});
-var writableStream = fs.createWriteStream(`./Final_Csv_File/${file_final}_final.csv`);
-
 // synch. way of reading through the files and push createReadStream for each file with it's path.
-let files = fs.readdirSync(`.${file_folder}`);
+let files = fs.readdirSync(`..${file_folder}`);
 let total_files = files.length;
 
-//creating a read stream which is used to 
+
+// These variables are for file_iterator, the mapped variables.
+const file_folder_map = "/Final_Csv_File/"
+let files_map = ['model_final.csv', 'batch_final.csv']
+let streams_map = [];
+let mapped_data = {};
+
+//creating a writable stream which writes to growthcurve_data csv file.
+var csvStream = csv.createWriteStream({headers: ["id", "batch_id", "model_id", "type"]});
+var writableStream = fs.createWriteStream(`../Final_Csv_File/${file_final}_final.csv`);
+
+
+//function to create the output data.
+let id = 1;
 function outputData() {
   MultiStream(streams).pipe(csv())
                       .on('data', function(data) {
@@ -36,14 +46,31 @@ function outputData() {
                         results.forEach((data) => {
                             if(data[0] == "" || data[0] == "batch.name") {}
                             else {
-                                //remove first element from the array and write it to a new file.
-                                data.shift();
-                                csvStream.write({batch: data[0], model_id: data[1], type: data[2]});
+                                csvStream.write({id: id++, batch_id: mapped_data[data[1]], model_id: mapped_data[data[2]], type: data[3]});
                             }
                         })
                         console.log("Done with the conversion");
                       })
 }
 
-//calling function to loop through all the files in folder and gives the output
-file_reader(files, total_files, file_folder, outputData, streams)
+// function to create a new promise and calling the file iterator function.
+let callIterator = () => {
+  return new Promise((resolve, reject) => {
+    file_iterator(files_map, file_folder_map, streams_map, mapped_data, (response) => {
+        if(Object.entries(response).length === 0 && response.constructor === Object) {
+          reject('Error')
+        } else {
+          resolve(mapped_data)
+        }
+      }
+    )
+  })
+}
+
+// calling the file reader function when the file iterator has been resolved or rejected.
+callIterator().then(() => {
+                      file_reader(files, total_files, file_folder, outputData, streams)
+                    })
+              .catch(error => {
+                  console.log(error);
+              })
