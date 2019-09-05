7f089a315ff6d63a35913e6b7a2c4f946c0a1ac2
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-05 14:56:37 -0400

    changes to seeding files

--- db/seeds/08_sequencing.js
+++ db/seeds/08_sequencing.js
@@ -2,15 +2,15 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/genes_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/sequencings_final.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
 
 
 exports.seed = function(knex, Promise) {
-  return knex('genes').del()
+  return knex('sequencing').del()
         .then(function() {
-          return knex('genes').insert(dataObj);
+          return knex('sequencing').insert(dataObj);
         });
 };
