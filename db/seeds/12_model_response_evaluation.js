d95889ad9598fc02e272e5c63cc5da484641bef7
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-05 17:32:09 -0400

    added new seeding files

--- db/seeds/12_model_response_evaluation.js
+++ db/seeds/12_model_response_evaluation.js
@@ -2,15 +2,15 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/response_evaluation_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/model_response_evaluation_final.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
 
 
 exports.seed = function(knex, Promise) {
-  return knex('response_evaluation').del()
+  return knex('model_response').del()
         .then(function() {
-          return knex('response_evaluation').insert(dataObj);
+          return knex('model_response').insert(dataObj);
         });
 };
