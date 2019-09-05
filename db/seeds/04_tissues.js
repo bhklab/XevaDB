28efd6b4e92e90441993127f87bc5c482aab2990
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-05 14:23:08 -0400

    changes to files

--- db/seeds/04_tissues.js
+++ db/seeds/04_tissues.js
@@ -2,15 +2,15 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/dataset_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/tissues_final.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
 
 
 exports.seed = function(knex, Promise) {
-  return knex('datasets').del()
+  return knex('tissues').del()
         .then(function() {
-          return knex('datasets').insert(dataObj);
+          return knex('tissues').insert(dataObj);
         });
 };
--- knexfile.js
+++ knexfile.js
@@ -6,10 +6,14 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : 'xevadb-database.mysql.database.azure.com',
-      user : 'bhklab@xevadb-database',
-      password : 'canada@24',
-      database : 'xevadb_latest_13_aug'
+      host: 'localhost',
+      user: 'root',
+      password: 'Divya@24',
+      database: 'xevadb_latest'
+      //host : 'xevadb-database.mysql.database.azure.com',
+      //user : 'bhklab@xevadb-database',
+      //password : 'canada@24',
+      //database : 'xevadb_latest_13_aug'
     },
     migrations: {
       directory: __dirname + '/db/migrations',
