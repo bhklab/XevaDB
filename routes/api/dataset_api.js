74fcad02a8a2c0dd6328f6b83474e85fccea4629
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-06 16:33:48 -0400

    started rewriting APIs

--- routes/api/dataset_api.js
+++ routes/api/dataset_api.js
@@ -1,43 +1,21 @@
 const knex = require('../../db/knex1');
 
-// get all the data from drug table.
-const getDatasetTableData = function(req,res) {
+// get all the data from the dataset table.
+const getDatasets = function(request, response) {
     knex.select()
         .from('datasets')
-        .then((dataset) => res.status(200).json({
+        .then((dataset) => response.status(200).json({
             status: 'success',
             data: dataset
         }))
-        .catch((error) => res.status(500).json({
+        .catch((error) => response.status(500).json({
             status: 'could not find data from dataset table, getDatasetTableData',
             data: error
         }))
 }
 
-
-// get all the count of distinct patient ids for a particular dataset, this uses the modelinformation table.
-const getDatasetDistinctPatient = function(req,res) {
-    knex.countDistinct('model_information.patient_id as patient_id')
-        .select('dataset_name', 'dataset_id')
-        .from('model_information')
-        .leftJoin(
-            'datasets',
-            'model_information.dataset',
-            'datasets.dataset_id'
-        )
-        .groupBy('dataset')
-        .then((dataset) => res.status(200).json({
-            status: 'success',
-            data: dataset
-        }))
-        .catch((error) => res.status(500).json({
-            status: 'could not find data from dataset table, getDatasetTableData',
-            data: error
-        }))
-}
 
 
 module.exports = {
-    getDatasetTableData,
-    getDatasetDistinctPatient
+    getDatasets
 }
