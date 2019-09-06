27abb1edd903e729d60dfba21edb3b9bc59720c7
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-06 16:33:48 -0400

    started rewriting APIs

--- routes/api/drug_api.js
+++ routes/api/drug_api.js
@@ -1,9 +1,9 @@
 const knex = require('../../db/knex1');
 
 // get all the data from drug table.
-const getDrugTableData = function(req,res) {
+const getDrugs = function(req,res) {
     knex.select()
-        .from('drug')
+        .from('drugs')
         .then((drug) => {
             res.send(drug);
         })
@@ -14,32 +14,7 @@ const getDrugTableData = function(req,res) {
 }
 
 
-// this will get the patients/model ids belonging a class of drug.
-const getDrugClassName = function(req, res) {
-    knex('model_information')
-        .count('model_information.patient_id as model_ids')
-        .leftJoin(
-            'drug',
-            'model_information.drug',
-            'drug.drug_id'
-        )
-        .select('class_name')
-        .groupBy('class_name')
-        .then((class_name) => res.status(200).json({
-            status: 'success',
-            data: class_name
-          }))
-        .catch((error) => res.status(500).json({
-            status: 'could not find data from drug table, getDrugData',
-            data: error
-        }))
-}
-
-
-
-
 
 module.exports = {
-    getDrugTableData,
-    getDrugClassName
+    getDrugs
 }
