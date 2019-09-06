da93f980448f41d4f80d7d64e5c50a8e3cbc7f34
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-06 18:14:43 -0400

    new functions

---
+++ routes/api/gene_api.js
@@ -0,0 +1,22 @@
+const knex = require('../../db/knex1');
+
+
+// get all the data from the genes table.
+const getGenes = function(request, response) {
+    knex.select()
+        .from('genes')
+        .then((gene) => response.status(200).json({
+            status: 'success',
+            data: gene
+        }))
+        .catch((error) => response.status(500).json({
+            status: 'could not find data from genes table, getGenes',
+            data: error
+        }))
+}
+
+
+
+module.exports = {
+    getGenes
+}
