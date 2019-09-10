286df499b0f3c8d22c5975b83dbf54cb5a606076
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-10 12:45:20 -0400

    new functions

--- routes/api/dataset_api.js
+++ routes/api/dataset_api.js
@@ -16,7 +16,30 @@ const getDatasets = function(request, response) {
 }
 
 
+// get all the count of distinct patient ids grouped by datasets, this uses the modelinformation table.
+const getPatientsGroupedByDataset = function(request, response) {
+    knex.countDistinct('model_information.patient_id as patient_id')
+        .select('model_information.dataset_id', 'datasets.dataset_name')
+        .from('model_information')
+        .leftJoin(
+            'datasets',
+            'model_information.dataset_id',
+            'datasets.dataset_id'
+        )
+        .groupBy('dataset_id')
+        .then((dataset) => response.status(200).json({
+            status: 'success',
+            data: dataset
+        }))
+        .catch((error) => response.status(500).json({
+            status: 'could not find data from dataset table, getDatasetTableData',
+            data: error
+        }))
+}
+
+
 
 module.exports = {
-    getDatasets
+    getDatasets,
+    getPatientsGroupedByDataset
 }
