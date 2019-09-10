a53c3a15592d00b65868057ca8cc86043619352c
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-10 14:45:32 -0400

    function to get patient ids grouped by tissue name

--- routes/api/tissue_api.js
+++ routes/api/tissue_api.js
@@ -16,7 +16,31 @@ const getTissues = function(request, response) {
 }
 
 
+// this is grouping different patientids based on the different tissues.
+const getPatientsGroupedByTissue= function(request, response) {
+    knex.select('model_information.tissue_id', 'tissues.tissue_name')
+        .count('model_information.patient_id as total')
+        .groupBy('model_information.tissue_id')
+        .from('model_information')
+        .leftJoin(
+            'tissues',
+            'model_information.tissue_id',
+            'tissues.tissue_id'
+        )
+        .whereNot('model_information.tissue_id','')
+        .then((tissue) => response.status(200).json({
+            status: 'success',
+            data: tissue
+          }))
+        .catch((error) => response.status(500).json({
+            status: 'could not find tissue',
+            data: error
+        }))
+}
+
+
 
 module.exports = {
-    getTissues
+    getTissues,
+    getPatientsGroupedByTissue
 }
