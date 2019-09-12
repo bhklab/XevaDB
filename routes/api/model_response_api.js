2ab239c2ed5fb00358b5938815d8d3f9c653adae
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-12 19:08:56 -0400

    model response api with new data, needs to fix an issue

--- routes/api/model_response_api.js
+++ routes/api/model_response_api.js
@@ -6,26 +6,32 @@ const getModelResponseBasedOnDataset = function(req,res) {
     let param_dataset = req.params.dataset
 
     function distinctPatient() {
-        return this.distinct('patient_id')
+        return this.distinct('model_id')
                     .from('model_information')
                     .where({
                         dataset_id: param_dataset
                     })
     }
 
-    knex.select('models.model', 'drugs.drug_name', 'value')
+    knex.select('patients.patient', 'drugs.drug_name', 'value')
         .from('model_response')
-        .leftJoin(
-            'models',
-            'model_response.model_id',
-            'models.model_id'
-        )
         .leftJoin(
             'drugs',
             'model_response.drug_id',
             'drugs.drug_id'
         )
+        .leftJoin(
+            'model_information',
+            'model_response.model_id',
+            'model_information.model_id'
+        )
+        .leftJoin(
+            'patients',
+            'model_information.patient_id',
+            'patients.patient_id'
+        )
         .whereIn('model_response.model_id', distinctPatient)
+        .andWhere('model_response.response_type', 'mRECIST')
         .then((row) => {
             console.log(row)
             let drug = ''
@@ -35,16 +41,16 @@ const getModelResponseBasedOnDataset = function(req,res) {
             usersRows = JSON.parse(JSON.stringify(row));
             usersRows.forEach(element => {
                 if (element.drug_name === drug) {
-                    data[value-1][element.model] = element.value
+                    data[value-1][element.patient] = element.value
                 } 
                 else if (element.drug_name === 'untreated') {
-                    untreated[element.model] = element.value
+                    untreated[element.patient] = element.value
                 }
                 else {
                     drug = element.drug_name
                     data.push({})
                     data[value]['drug_name'] = element.drug_name
-                    data[value][element.model] = element.value
+                    data[value][element.patient] = element.value
                     value += 1
                 }
             })
