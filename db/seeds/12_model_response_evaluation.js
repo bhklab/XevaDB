8714cc1c9a43068d7feafcd442cacc9839fb7ae5
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-13 16:58:57 -0400

    another minor change

--- db/seeds/12_model_response_evaluation.js
+++ db/seeds/12_model_response_evaluation.js
@@ -2,7 +2,7 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/model_response_evaluation_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/final_model_response.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
