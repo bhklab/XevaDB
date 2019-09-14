a2d7323ea35ba9f23b64fe797f62dce843e343d3
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-09-13 20:00:09 -0400

    added new code for model response

--- routes/api/model_response_api.js
+++ routes/api/model_response_api.js
@@ -13,7 +13,19 @@ const getModelResponseBasedOnDataset = function(req,res) {
                     })
     }
 
-    knex.select('patients.patient', 'drugs.drug_name', 'value')
+    let promise1 = knex('model_information')
+                        .distinct('patients.patient')
+                        .from('model_information')
+                        .leftJoin(
+                            'patients',
+                            'model_information.patient_id',
+                            'patients.patient_id'
+                        )
+                        .where({
+                            dataset_id: param_dataset
+                        })
+
+    let promise2 = knex.select('patients.patient', 'drugs.drug_name', 'value')
         .from('model_response')
         .leftJoin(
             'drugs',
@@ -32,32 +44,46 @@ const getModelResponseBasedOnDataset = function(req,res) {
         )
         .whereIn('model_response.model_id', distinctPatient)
         .andWhere('model_response.response_type', 'mRECIST')
-        .then((row) => {
-            console.log(row)
-            let drug = ''
-            let data = []
-            let untreated = {Drug:'untreated'}
-            let value = 0
-            usersRows = JSON.parse(JSON.stringify(row));
-            usersRows.forEach(element => {
-                if (element.drug_name === drug) {
-                    data[value-1][element.patient] = element.value
-                } 
-                else if (element.drug_name === 'untreated') {
-                    untreated[element.patient] = element.value
-                }
-                else {
-                    drug = element.drug_name
-                    data.push({})
-                    data[value]['drug_name'] = element.drug_name
-                    data[value][element.patient] = element.value
-                    value += 1
-                }
-            })
-            if(Object.entries(untreated).length === 1 && untreated.constructor === Object) {}
-            else {data.unshift(untreated)}
-            res.send(data)
-        })
+        .orderBy('drug_name')
+
+        Promise.all([promise1, promise2])
+                .then((row) => {
+                    let drug = ''
+                    let data = []
+                    let untreated = {Drug:'untreated'}
+                    let value = 0
+                    let patient = []
+                    
+                    //this will create enteries for heatmap.
+                    usersRows = JSON.parse(JSON.stringify(row[1]));
+                    usersRows.forEach(element => {
+                        if (element.drug_name === drug) {
+                            data[value-1][element.patient] = element.value
+                        } 
+                        else if (element.drug_name === 'untreated') {
+                            untreated[element.patient] = element.value
+                        }
+                        else {
+                            drug = element.drug_name
+                            data.push({})
+                            data[value]['Drug'] = element.drug_name
+                            data[value][element.patient] = element.value
+                            value += 1
+                        }
+                    })
+                    if(Object.entries(untreated).length === 1 && untreated.constructor === Object) {}
+                    else {data.unshift(untreated)}
+
+                    //array of all the patients belonging to a particular dataset.
+                    patientRows = usersRows = JSON.parse(JSON.stringify(row[0]));
+                    patient = patientRows.map(element => {
+                        return element.patient
+                    })
+                    data.push(patient)
+
+                    //sending the response.
+                    res.send(data)
+                })
 }
 
 
