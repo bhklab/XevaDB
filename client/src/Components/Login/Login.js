81e101decd2a0100f097cb6c5f0e5ec97aba4584
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-10-09 17:49:49 -0400

    changed login page

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,118 +1,117 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {Paper, FormStyle, SubmitStyle} from './LoginStyle'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        BHKLab
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username : '',
+      password : ''
+    }
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+
+  handleUserChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+
+  handlePasswordChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      username: event.target.password
+    })
+  }
+
+  handleSubmit = (event) => {
+    
+  }
+
+
+  render() {
+    return (
+      <Container component="main" maxWidth="xs">
+        <Paper>
+
+          <Typography component="h1" variant="h5" style={{color:'#3f51b5'}}>
+            Sign in
+          </Typography>
+
+          <FormStyle>
+            <form noValidate>
+
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Username"
+                name="username"
+                autoFocus
+                onChange={this.handleUserChange}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.handlePasswordChange}
+              />
+
+              <SubmitStyle>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                Sign In
+              </Button>
+              </SubmitStyle>
+              
+              <Grid container style={{marginTop: '5px'}}>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+
+            </form>
+          </FormStyle>
+
+        </Paper>
+      </Container>
+    )
+  }
+
 }
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
-export default function Login() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+export default Login
---
+++ client/src/Components/Login/LoginStyle.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const Paper = styled.div`
+margin-top: 30vh;
+display: flex;
+flex-direction: column;
+align-items: center
+`
+
+const FormStyle = styled.form`
+width: 100%;
+margin-top: 2.5vh;
+`
+
+const SubmitStyle = styled.div`
+margin-top: 2vh;
+`
+
+export {
+    Paper,
+    FormStyle,
+    SubmitStyle
+}
