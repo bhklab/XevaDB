cb9ad3cd592c949936e44f4f91b1bfb24bd30ad6
gangeshberi <gangeshberi1@gmail.com>
2019-10-16 00:58:53 -0400

    knexfile.js modification

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'Divya@24',
-      database: 'xevadb_latest1'
-      // host : 'xevadb-database.mysql.database.azure.com',
-      // user : 'bhklab@xevadb-database',
-      // password : 'canada@24',
-      // database : 'xevadb_latest'
+      // host: 'localhost',
+      // user: 'root',
+      // password: 'Divya@24',
+      // database: 'xevadb_latest1'
+      host : 'xevadb-database.mysql.database.azure.com',
+      user : 'bhklab@xevadb-database',
+      password : 'canada@24',
+      database : 'xevadb_latest'
     },
     migrations: {
       directory: __dirname + '/db/migrations',
