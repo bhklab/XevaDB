291150201826536f52ca0d0775790b2ca702a385
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-10-23 15:50:19 -0400

    changes

---
+++ db/migrations/20191022144053_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', (table) => {
+        table.increments('user_id')
+             .primary();
+        table.string('user_name')
+             .notNullable();
+        table.string('user_pwd')
+             .notNullable();
+    })
+};
+
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('users');
+};
