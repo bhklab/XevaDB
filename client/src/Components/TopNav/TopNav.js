519523fe11c64a7d7edfff946430db86734ef559
gangeshberi <gangeshberi1@gmail.com>
2019-10-25 13:20:30 -0400

    changing top nav for login and logout

--- client/src/Components/TopNav/TopNav.js
+++ client/src/Components/TopNav/TopNav.js
@@ -2,8 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {HeaderStyle, LogoStyle, LinkStyle} from './NavStyle'
 import logo from '../../images/logo.png';
+import Button from '@material-ui/core/Button'
 
 class TopNav extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLogged : 'Login',
+            isLink : '/login'
+        }
+    }
+
+    componentWillMount() {
+        localStorage.getItem('user') 
+        ? this.setState({isLogged : 'Logout', isLink : '/'}) 
+        : this.setState({isLogged : 'Login'})
+    }
+
+    isLoggedIn = (event) => {
+        if(this.state.isLogged === 'Logout') {
+            localStorage.removeItem('user')
+            this.setState({isLogged : 'Login', isLink : '/login'})
+        }
+    }
+
     render() {
         return (
             <HeaderStyle>
@@ -16,6 +39,17 @@ class TopNav extends React.Component {
                     <Link to='/drugs'> Drugs </Link>
                     <Link to='/tissues'> Tissues </Link>
                     <Link to='/doc'> Documentation </Link>
+                    <Link to={`${this.state.isLink}`}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={this.isLoggedIn}
+                        >
+                            {this.state.isLogged}
+                        </Button>
+                    </Link>
                 </LinkStyle>         
             </HeaderStyle>
         )
