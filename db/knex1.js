be37317b91be8991aa25fd0a7345f8b8de355932
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-01 10:59:34 -0400

    minor changes

--- db/knex1.js
+++ db/knex1.js
@@ -1,3 +1,3 @@
-var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile.js')[environment];
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
 module.exports = require('knex')(config);
