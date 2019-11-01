589cf11160d36323c4490fde624536301bebca40
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-01 12:54:30 -0400

    more changes

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
+/* eslint-disable no-console */
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-//const cors = require('cors');
+// const cors = require('cors');
 
 // setting the path of the router file to variable so that we can use all the routes from it.
 const router = require('./routes/router.js');
 
-//Bodyparser Middleware
+// Bodyparser Middleware
 app.use(bodyParser.json());
 
-//app.use(cors({origin: 'http://localhost:3000'}));
+// app.use(cors({origin: 'http://localhost:3000'}));
 
-//this will set/use our api to initial path of /api.
+// this will set/use our api to initial path of /api.
 app.use('/api', router);
 
 
-//use 5000 port no. for server.
+// use 5000 port no. for server.
 const port = process.env.PORT || 5000;
 
 
 // build to serve static files.
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get(`/*`, (req, res) => {
-    res.sendFile(`index.html`, { root: `./client/build` });
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: './client/build' });
 });
 
 
 app.listen(port, () => {
     console.log('Server Started');
-})
+});
--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,37 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Update with your config settings.
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PWD,
-      database : process.env.DB_NAME
+    development: {
+        client: 'mysql',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: `${__dirname}/db/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/db/seeds`,
+        },
     },
-    migrations: {
-      directory: `${__dirname}/db/migrations`,
+    production: {
+        client: 'mysql',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: `${__dirname}/db/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/db/seeds`,
+        },
     },
-    seeds: {
-      directory: `${__dirname}/db/seeds`,
-    },
-  },
-  production: {
-    client: 'mysql',
-    connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PWD,
-      database : process.env.DB_NAME
-    },
-    migrations: {
-      directory: `${__dirname}/db/migrations`,
-    },
-    seeds: {
-      directory: `${__dirname}/db/seeds`,
-    },
-  }
- };
+};
