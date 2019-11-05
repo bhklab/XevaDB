2ec263b034c81881b3e9e41bc362c4712dcd55cb
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-04 19:06:12 -0500

    more more changes

--- client/src/Components/Search/SearchResultOncoprint.js
+++ client/src/Components/Search/SearchResultOncoprint.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
@@ -12,16 +13,16 @@ class SearchResultOncoprint extends React.Component {
         // setting the states for the data.
         this.state = {
             threshold: 0,
-            hmap_patients: [],
-            genes_mut: [],
-            genes_rna: [],
-            genes_cnv: [],
-            patient_mut: [],
-            patient_rna: [],
-            patient_cnv: [],
-            data_mut: [],
-            data_rna: [],
-            data_cnv: [],
+            hmapPatients: [],
+            genesMut: [],
+            genesRna: [],
+            genesCnv: [],
+            patientMut: [],
+            patientRna: [],
+            patientCnv: [],
+            dataMut: [],
+            dataRna: [],
+            dataCnv: [],
             dimensions: {},
             margin: {},
         };
@@ -34,40 +35,40 @@ class SearchResultOncoprint extends React.Component {
         const { datasetParam } = this.props;
         const { geneParam } = this.props;
         const { genomicsParam } = this.props;
-        const query_data = [];
+        const queryData = [];
 
         // making get requests based on the genomics parameters.
         // sequence is important (first cnv or mutation) then rnaseq has to be pushed.
         if (genomicsParam.includes('CNV') || genomicsParam.includes('Mutation')) {
-            query_data.push(axios.get(`/api/v1/mutation?genes=${geneParam}&dataset=${datasetParam}`));
+            queryData.push(axios.get(`/api/v1/mutation?genes=${geneParam}&dataset=${datasetParam}`));
         }
         if (genomicsParam.includes('RNASeq')) {
-            query_data.push(axios.get(`/api/v1/rnaseq?genes=${geneParam}&dataset=${datasetParam}`));
+            queryData.push(axios.get(`/api/v1/rnaseq?genes=${geneParam}&dataset=${datasetParam}`));
         }
 
-        Promise.all([...query_data])
+        Promise.all([...queryData])
             .then((response) => {
                 this.updateResults(response, genomicsParam);
             });
     }
 
     updateResults(result, genomicsParam) {
-        const genomics_value = genomicsParam.split(',');
+        const genomicsValue = genomicsParam.split(',');
         const genomics = [];
 
         // temporary solution to remove cnv if everything is selected.
-        if ((genomics_value.includes('Mutation') || genomics_value.includes('CNV'))) {
+        if ((genomicsValue.includes('Mutation') || genomicsValue.includes('CNV'))) {
             genomics.push('Mutation');
         }
-        if (genomics_value.includes('RNASeq')) {
+        if (genomicsValue.includes('RNASeq')) {
             genomics.push('RNASeq');
         }
 
         // grab the last array(patient array) from result[0]
         const dataset = result[0].data;
-        let hmap_patients = [];
+
         // grabbing the total patients from hmap.
-        hmap_patients = dataset.pop();
+        const hmapPatients = dataset.pop();
 
         // removing last element from each array element of result
         if (result.length > 1) {
@@ -78,7 +79,8 @@ class SearchResultOncoprint extends React.Component {
             });
         }
 
-        // setting patients genes and data for each of mutation, cnv and rna (given they are present)
+        // setting patients genes and data for each of mutation,
+        // cnv and rna (given they are present)
         const patient = {};
         const genes = {};
         const data = {};
@@ -87,35 +89,43 @@ class SearchResultOncoprint extends React.Component {
             const val = value.substring(0, 3).toLowerCase();
 
             // setting patients
-            const patient_id = Object.keys(result[i].data[0]).filter((value) => {
-                let return_value = '';
+            const patientId = Object.keys(result[i].data[0]).filter((value) => {
+                let returnValue = '';
                 if (value !== 'gene_id') {
-                    return_value = value;
+                    returnValue = value;
                 }
-                return return_value;
+                return returnValue;
             });
-            patient[`patient_${val}`] = patient_id;
+            patient[`patient_${val}`] = patientId;
 
             // genes
-            const gene_id = result[i].data.map((data) => data.gene_id);
-            genes[`genes_${val}`] = gene_id;
+            const geneId = result[i].data.map((data) => data.gene_id);
+            genes[`genes_${val}`] = geneId;
 
             // data
             data[`data_${val}`] = result[i].data;
         });
 
+        // destructuring the states and prop.
+        const {
+            dataMut, dataRna, dataCnv,
+            genesMut, genesRna, genesCnv,
+            patientMut, patientRna, patientCnv,
+        } = this.state;
+        const { threshold } = this.props;
+
         this.setState({
-            threshold: this.props.threshold,
-            hmap_patients,
-            patient_mut: patient.patient_mut === undefined ? this.state.patient_mut : patient.patient_mut,
-            patient_rna: patient.patient_rna === undefined ? this.state.patient_rna : patient.patient_rna,
-            patient_cnv: patient.patient_cnv === undefined ? this.state.patient_cnv : patient.patient_cnv,
-            genes_mut: genes.genes_mut === undefined ? this.state.genes_mut : genes.genes_mut,
-            genes_rna: genes.genes_rna === undefined ? this.state.genes_rna : genes.genes_rna,
-            genes_cnv: genes.genes_cnv === undefined ? this.state.genes_cnv : genes.genes_cnv,
-            data_mut: data.data_mut === undefined ? this.state.data_mut : data.data_mut,
-            data_rna: data.data_rna === undefined ? this.state.data_rna : data.data_rna,
-            data_cnv: data.data_cnv === undefined ? this.state.data_cnv : data.data_cnv,
+            threshold,
+            hmapPatients,
+            patientMut: patient.patient_mut === undefined ? patientMut : patient.patient_mut,
+            patientRna: patient.patient_rna === undefined ? patientRna : patient.patient_rna,
+            patientCnv: patient.patient_cnv === undefined ? patientCnv : patient.patient_cnv,
+            genesMut: genes.genes_mut === undefined ? genesMut : genes.genes_mut,
+            genesRna: genes.genes_rna === undefined ? genesRna : genes.genes_rna,
+            genesCnv: genes.genes_cnv === undefined ? genesCnv : genes.genes_cnv,
+            dataMut: data.data_mut === undefined ? dataMut : data.data_mut,
+            dataRna: data.data_rna === undefined ? dataRna : data.data_rna,
+            dataCnv: data.data_cnv === undefined ? dataCnv : data.data_cnv,
             dimensions: { height: 35, width: 20 },
             margin: {
                 top: 50, right: 200, bottom: 100, left: 250,
@@ -126,7 +136,8 @@ class SearchResultOncoprint extends React.Component {
 
     render() {
         const {
-            dimensions, margin, threshold,
+            dimensions, margin, threshold, dataMut, dataRna, dataCnv, hmapPatients,
+            genesMut, genesRna, genesCnv, patientMut, patientRna, patientCnv,
         } = this.state;
         return (
             <Oncoprint
@@ -134,24 +145,24 @@ class SearchResultOncoprint extends React.Component {
                 dimensions={dimensions}
                 margin={margin}
                 threshold={Number(threshold)}
-                hmap_patients={this.state.hmap_patients}
-                genes_mut={this.state.genes_mut.length ? this.state.genes_mut : this.state.genes_cnv}
-                genes_rna={this.state.genes_rna}
-                patient_mut={this.state.patient_mut.length ? this.state.patient_mut : this.state.patient_cnv}
-                patient_rna={this.state.patient_rna}
-                data_mut={this.state.data_mut.length ? this.state.data_mut : this.state.data_cnv}
-                data_rna={this.state.data_rna}
+                hmap_patients={hmapPatients}
+                genes_mut={genesMut.length ? genesMut : genesCnv}
+                genes_rna={genesRna}
+                patient_mut={patientMut.length ? patientMut : patientCnv}
+                patient_rna={patientRna}
+                data_mut={dataMut.length ? dataMut : dataCnv}
+                data_rna={dataRna}
             />
         );
     }
 }
 
 
-SearchResultOncoprint.propTypes = {
-    datasetParam: PropTypes.string.isRequired,
-    geneParam: PropTypes.string.isRequired,
-    genomicsParam: PropTypes.string.isRequired,
-    threshold: PropTypes.string.isRequired,
+SearchResultOncoprint.propTypes = {
+    datasetParam: PropTypes.string.isRequired,
+    geneParam: PropTypes.string.isRequired,
+    genomicsParam: PropTypes.string.isRequired,
+    threshold: PropTypes.string.isRequired,
 };
 
 
