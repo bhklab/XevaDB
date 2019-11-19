8eb37dae8c13f81c05ff9c3b6193860a6bce139b
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-19 17:17:36 -0500

    new build

--- client/build/precache-manifest.17c4afa9bddaa233719515ba7a3b7a2b.js
+++ client/build/precache-manifest.17c4afa9bddaa233719515ba7a3b7a2b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "da741e2e87a39e8882abbcef94480a6e",
+    "revision": "5ea59a7e7cfc7551ee1398849fc31020",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1d313419.chunk.js"
   },
   {
-    "revision": "e9a636a894210de9c643",
-    "url": "/static/js/main.f9811c48.chunk.js"
+    "revision": "0d691b3b28ce8afb8813",
+    "url": "/static/js/main.ad13ab01.chunk.js"
   },
   {
     "revision": "875d570ce1b6fc43afc6",
