cb635a3e4766135b3887af4e741a3503c68c4a53
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-22 19:18:48 -0500

    [Ongoing Changes] dataConversion

--- db/migrations/20190820185731_batch_response.js
+++ db/migrations/20190820185731_batch_response.js
@@ -3,12 +3,6 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('batch_response', (table) => {
         table.increments('id')
             .primary();
-        table.integer('drug_id')
-            .notNullable()
-            .unsigned()
-            .references('drug_id')
-            .inTable('drugs')
-            .index();
         table.integer('batch_id')
             .notNullable()
             .unsigned()
--- db/seeds/13_batch_response_evaluation.js
+++ db/seeds/13_batch_response_evaluation.js
@@ -2,7 +2,7 @@ const csv = require('csvjson');
 const fs = require('fs');
 const path = require('path');
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/model_response_evaluation_final.csv');
+const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/batch_response_final.csv');
 
 const file = fs.readFileSync(file_location, 'utf8');
 const dataObj = csv.toObject(file);
@@ -10,7 +10,5 @@ const dataObj = csv.toObject(file);
 
 exports.seed = function (knex, Promise) {
     return knex('model_response').del()
-        .then(() => {
-          return knex('model_response').insert(dataObj);
-        });
+        .then(() => knex('model_response').insert(dataObj));
 };
