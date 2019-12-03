bcd8adc0971dd0bed1b2de7144ff6aa971cf0724
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-03 10:56:10 -0500

    changes going on

--- db/seeds/08_sequencing.js
+++ db/seeds/08_sequencing.js
@@ -2,7 +2,7 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/sequencings_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_csv_file/sequencings_final.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
--- db/seeds/10_batch_information.js
+++ db/seeds/10_batch_information.js
@@ -2,7 +2,7 @@ const csv = require ('csvjson')
 const fs = require ('fs')
 const path = require('path')
 
-const file_location = path.join(__dirname, '../../data_conversion/Final_Csv_File/batch_information_final.csv')
+const file_location = path.join(__dirname, '../../data_conversion/Final_csv_file/batch_information_final.csv')
 
 const file = fs.readFileSync(file_location , 'utf8')
 const dataObj = csv.toObject(file)
