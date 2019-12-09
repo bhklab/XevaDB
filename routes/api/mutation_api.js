648f30a8fa357a6c8d115c15d256de0527672cd5
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-09 15:06:03 -0500

    Changes to various API for mapping patient ids and sequencing ids

--- routes/api/mutation_api.js
+++ routes/api/mutation_api.js
@@ -6,7 +6,7 @@ const knex = require('../../db/knex1');
 
 // This will get the mutation for the selected dataset id.
 const getMutationBasedOnDataset = function (request, response) {
-    const param_dataset = request.params.dataset;
+    const paramDataset = request.params.dataset;
 
     // get the distinct patients or total patients from model information table.
     // as some patient ids are missing from oncoprint
@@ -20,27 +20,16 @@ const getMutationBasedOnDataset = function (request, response) {
             'patients.patient_id',
         )
         .where({
-            dataset_id: param_dataset,
+            dataset_id: paramDataset,
         });
 
-    // to get the number of distinct patient ids.
-    const distinctPatient = knex.distinct('modelid_moleculardata_mapping.sequencing_uid')
-        .from('model_information')
-        .leftJoin(
-            'modelid_moleculardata_mapping',
-            'model_information.model_id',
-            'modelid_moleculardata_mapping.model_id',
-        )
-        .where('model_information.dataset_id', param_dataset)
-        .andWhere('modelid_moleculardata_mapping.mDataType', 'mutation');
-
     // mutation data.
-    Promise.all([modelInformationDistinctPatient, distinctPatient])
+    modelInformationDistinctPatient
         .then((total) => {
-            const patientRows = JSON.parse(JSON.stringify(total[0]));
+            const patientRows = JSON.parse(JSON.stringify(total));
             const data = [];
             // grabbing the mutation data based on patients and limiting genes to 1-30.
-            knex.select('genes.gene_name', 'sequencing.sequencing_id', 'mutation.value')
+            knex.select('genes.gene_name', 'patients.patient', 'mutation.value')
                 .from('mutation')
                 .rightJoin(
                     'genes',
@@ -48,11 +37,29 @@ const getMutationBasedOnDataset = function (request, response) {
                     'genes.gene_id',
                 )
                 .leftJoin(
-                    'sequencing',
+                    'modelid_moleculardata_mapping',
                     'mutation.sequencing_uid',
+                    'modelid_moleculardata_mapping.sequencing_uid',
+                )
+                .leftJoin(
+                    knex.select()
+                        .from('model_information')
+                        .groupBy('model_information.patient_id')
+                        .as('model_information'),
+                    'modelid_moleculardata_mapping.model_id',
+                    'model_information.model_id',
+                )
+                .leftJoin(
+                    'patients',
+                    'model_information.patient_id',
+                    'patients.patient_id',
+                )
+                .leftJoin(
+                    'sequencing',
+                    'modelid_moleculardata_mapping.sequencing_uid',
                     'sequencing.sequencing_uid',
                 )
-                .whereIn('mutation.sequencing_uid', distinctPatient)
+                .where('model_information.dataset_id', paramDataset)
                 .andWhereBetween('mutation.gene_id', [1, 30])
                 .then((mutation_data) => {
                     let gene_id = '';
@@ -63,10 +70,10 @@ const getMutationBasedOnDataset = function (request, response) {
                             gene_id = element.gene_name;
                             data[i] = {};
                             data[i].gene_id = element.gene_name;
-                            data[i][element.sequencing_id] = element.value;
+                            data[i][element.patient] = element.value;
                             i++;
                         } else {
-                            data[i - 1][element.sequencing_id] = element.value;
+                            data[i - 1][element.patient] = element.value;
                         }
                     });
 
