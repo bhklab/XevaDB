54480beeef8f4580a5347d2def766c4a15265ef3
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-09 16:53:48 -0500

    Changes to APIs mapping sequencing ids and patient ids

--- routes/api/copy_number_variation_api.js
+++ routes/api/copy_number_variation_api.js
@@ -71,9 +71,9 @@ const getCopyNumberVariationBasedOnDataset = function (request, response) {
 // this will get the copy_number_variation based on
 // dataset and drug query parameters.
 const getCopyNumberVariationBasedPerDatasetBasedOnGenes = function (request, response) {
-    const param_gene = request.query.genes;
-    const param_dataset = request.query.dataset;
-    const genes = param_gene.split(',');
+    const paramGene = request.query.genes;
+    const paramDataset = request.query.dataset;
+    const genes = paramGene.split(',');
 
     // get the distinct patients or total patients from model information table.
     // as some patient ids are missing from oncoprint
@@ -87,19 +87,8 @@ const getCopyNumberVariationBasedPerDatasetBasedOnGenes = function (request, res
             'patients.patient_id',
         )
         .where({
-            dataset_id: param_dataset,
+            dataset_id: paramDataset,
         });
-
-    // to get the number of distinct patient ids.
-    const distinctPatient = knex.distinct('modelid_moleculardata_mapping.sequencing_uid')
-        .from('model_information')
-        .leftJoin(
-            'modelid_moleculardata_mapping',
-            'model_information.model_id',
-            'modelid_moleculardata_mapping.model_id',
-        )
-        .where('model_information.dataset_id', param_dataset)
-        .andWhere('modelid_moleculardata_mapping.mDataType', 'cnv');
 
     // to get the gene list based on gene_id param.
     const gene_list = knex.select('gene_id')
@@ -107,16 +96,17 @@ const getCopyNumberVariationBasedPerDatasetBasedOnGenes = function (request, res
         .whereIn('gene_name', genes);
 
     // copy_number_variation data.
-    Promise.all([modelInformationDistinctPatient, distinctPatient, gene_list])
+    Promise.all([modelInformationDistinctPatient, gene_list])
         .then((row) => {
             const data = [];
             // patients
             const patientRows = JSON.parse(JSON.stringify(row[0]));
             // parsing the gene_list in order to get an array of genes.
-            let value = JSON.parse(JSON.stringify(row[2]));
+            let value = JSON.parse(JSON.stringify(row[1]));
             value = value.map((val) => val.gene_id);
+
             // grabbing the copy_number_variation data for the genes.
-            knex.select('genes.gene_name', 'sequencing.sequencing_id', 'copy_number_variation.value')
+            knex.select('genes.gene_name', 'patients.patient', 'copy_number_variation.value')
                 .from('copy_number_variation')
                 .rightJoin(
                     'genes',
@@ -124,11 +114,29 @@ const getCopyNumberVariationBasedPerDatasetBasedOnGenes = function (request, res
                     'genes.gene_id',
                 )
                 .leftJoin(
-                    'sequencing',
+                    'modelid_moleculardata_mapping',
                     'copy_number_variation.sequencing_uid',
+                    'modelid_moleculardata_mapping.sequencing_uid',
+                )
+                .leftJoin(
+                    knex.select()
+                        .from('model_information')
+                        .groupBy('model_information.patient_id')
+                        .as('model_information'),
+                    'modelid_moleculardata_mapping.model_id',
+                    'model_information.model_id',
+                )
+                .leftJoin(
+                    'patients',
+                    'model_information.patient_id',
+                    'patients.patient_id',
+                )
+                .leftJoin(
+                    'sequencing',
+                    'modelid_moleculardata_mapping.sequencing_uid',
                     'sequencing.sequencing_uid',
                 )
-                .whereIn('copy_number_variation.sequencing_uid', distinctPatient)
+                .where('model_information.dataset_id', paramDataset)
                 .whereIn('copy_number_variation.gene_id', value)
                 .then((copy_number_variation_data) => {
                     let gene_id = '';
@@ -139,10 +147,10 @@ const getCopyNumberVariationBasedPerDatasetBasedOnGenes = function (request, res
                             gene_id = element.gene_name;
                             data[i] = {};
                             data[i].gene_id = element.gene_name;
-                            data[i][element.sequencing_id] = element.value;
+                            data[i][element.patient] = element.value;
                             i++;
                         } else {
-                            data[i - 1][element.sequencing_id] = element.value;
+                            data[i - 1][element.patient] = element.value;
                         }
                     });
 
