e060c8466cf38fcd1c893f26783adf0b573d4cb6
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-25 13:30:08 -0500

    Minor Changes.

--- data_conversion/Scripts_Python/genes.py
+++ data_conversion/Scripts_Python/genes.py
@@ -12,25 +12,30 @@ files = [
             '../Initial_csv_file/copy_number_variation/copy_number_variation_TNBC.csv',
             '../Initial_csv_file/copy_number_variation/copy_number_variation_SU2C.csv',
             '../Initial_csv_file/mutation_data/mutation_data_SU2C.csv',
-            #'../Initial_csv_file/mutation_data/abc.csv'
+            #'../Initial_csv_file/abctest.csv'
         ]
 
 output_file = '../Final_csv_file/genes_final.csv'
+gene_list = {}
 unique_list = []
 value = 0
+counter = 1
 
 for file in files:
     with open(file, 'r') as read_file:
         for row in read_file:
             #print(row)
-            if(re.search(r'\bgene.id\b', row)):
+            row = (row.replace('"','').replace("\n", "")).replace("\r", "")
+            row = row.split(',')[1].upper()
+            if(re.search(r'\bGENE.ID\b', row)):
                     print('not useful')
-            else:
-                    row = (row.replace('"','').replace("\n", "")).replace("\r", "")
-                    row = row.split(',')[1]
-                    if (row not in unique_list and row.upper() not in unique_list and row.lower() not in unique_list):
-                            unique_list.append(row.upper())
-#print(unique_list)
+            elif(not gene_list.get(row)):
+                    gene_list[row] = counter
+                    counter+=1
+
+unique_list = gene_list.keys()
+unique_list.sort()
+
 
 with open(output_file, 'w') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter = ",")
