0356ac9eb6643b6a91cd2130e5f618196a5c1341
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-01-28 14:12:56 -0500

    declaring context to be used in the app.

---
+++ client/src/Components/Dataset/DatasetContext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PatientContext = React.createContext({
+    globalPatients: [],
+    setPatients: () => {},
+});
+
+const PatientProvider = PatientContext.Provider;
+const PatientConsumer = PatientContext.Consumer;
+
+export { PatientProvider, PatientConsumer };
+export default PatientContext;
