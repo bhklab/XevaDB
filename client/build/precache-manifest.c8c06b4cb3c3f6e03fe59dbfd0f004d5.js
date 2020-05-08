778c67d96c1f04f12e87d4710c11c9a4185e9ef1
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-05-07 21:06:02 -0400

    new build may 7th 2020

---
+++ client/build/precache-manifest.c8c06b4cb3c3f6e03fe59dbfd0f004d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c2058a2f1bc5972d690633073d9af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f7ab0bfef8f64d4b6e",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "35f7ab0bfef8f64d4b6e",
+    "url": "/static/js/2.3e9525d6.chunk.js"
+  },
+  {
+    "revision": "f3bd3fba18d4f751232a",
+    "url": "/static/js/main.6917f568.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "3eb67c7e31c8352852b69a513ce94a52",
+    "url": "/static/media/bgImg7.3eb67c7e.png"
+  },
+  {
+    "revision": "1de5f71c18bcf76d28fe16e2fd51f880",
+    "url": "/static/media/logo.1de5f71c.png"
+  }
+]);
