bdeb5df1fa9d623ffd02a762f8a76cc54a74693f
gangeshberi <gangeshberi@gmail.com>
2021-02-05 09:22:37 -0500

    added a new file to grab the data path.

---
+++ db/path.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+// const data_path = '../../XevaDI/output_data'
+const data_path = path.join(__dirname, '../../XevaDB/data_conversion/FinalCsvFiles/')
+
+module.exports = data_path;
