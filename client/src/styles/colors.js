5e9b1b297497ec7238c8a427a747fb5c5fbab2a8
gangeshberi <gangeshberi@gmail.com>
2021-02-22 12:19:35 -0500

    added `colors.js` to styles folder.

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,8 +1,14 @@
 export default {
-    blue: '#0033CC',
-    green: '#1a9850',
-    yellow: '#fed976',
-    red: '#e41a1c',
+    blue_heatmap: '#0033CC',
+    green_heatmap: '#1a9850',
+    yellow_heatmap: '#fed976',
+    red_heatmap: '#e41a1c',
+    amber_gradient: '#d8b365',
+    white_gradient: '#f7f7f7',
+    green_gradient: '#5ab4ac',
     pink_header: '#cd5686',
     blue_header: '#3453b0',
+    lightgray: '#D3D3D3',
+    black: '#000000',
+    white: '#ffffff',
 };
