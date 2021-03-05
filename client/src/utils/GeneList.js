78fa9ed95bb4fe68e17902637e41301827a9ffca
gangeshberi <gangeshberi@gmail.com>
2021-03-05 13:44:30 -0500

    minor update to `GeneList.js`.

--- client/src/utils/GeneList.js
+++ client/src/utils/GeneList.js
@@ -24,9 +24,6 @@ export const GeneList = [
     'General: Invasion and metastasis=MMP1 MMP2 MMP3 MMP7 MMP9 MMP10 MMP11 MMP12 MMP13 MMP14 MMP15 MMP16 MMP17 MMP19 MMP21 MMP23B MMP24 MMP25 MMP26 MMP27 MMP28 ITGB3 ITGAV PTK2 CDH1 SPARC WFDC2',
 ];
 
-export const OncoprintGenes = ['BRAF', 'BRCA1', 'ERBB2', 'FGFR1', 'JAK1', 'JAK2', 'KIT', 'KRAS', 'KRAS', 'MAPK1', 'MAPK1', 'MDM2', 'MDM4', 'MTOR', 'MYC', 'NFKB1', 'NOTCH1', 'NRAS', 'PIK3CA', 'PTEN', 'SOX9', 'TP53'];
-
 export default {
     GeneList,
-    OncoprintGenes,
 };
