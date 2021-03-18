506a89ad68f4730c94ab218e8535b9737c0d8e99
gangeshberi <gangeshberi@gmail.com>
2021-03-18 18:36:49 -0400

    updated  component.

--- client/src/Components/Utils/ExportPng.js
+++ client/src/Components/Utils/ExportPng.js
@@ -6,21 +6,25 @@ import downloadIcon from '../../images/download.svg';
 
 const StyledButton = styled.div`
     font-weight: 500;
+    display: inline !important;
+    align-self: flex-end;
+    margin-right: 70px;
+    margin-bottom: 30px;
     
     button {
-      background-color: ${colors.blue_header} !important;
-      color: ${colors.white} !important;
-      padding: 6px !important;
-      padding-left: 10px !important;
-      margin-right: 5px;
-      border-radius: 6px;
-      border: 1px;
-      font-size: 1.0em;
-      :hover {
+        background-color: ${colors.blue_header} !important;
+        color: ${colors.white} !important;
+        padding: 6px !important;
+        padding-left: 10px !important;
+        margin-right: 5px;
+        border-radius: 6px;
+        border: 1px;
+        font-size: 1.0em;
+        :hover {
         color: ${colors.blue_header} !important;
         background-color: ${colors.lightgray} !important;
         cursor: pointer;
-      }
+        }
     }
     img {
         display: inline-block;
