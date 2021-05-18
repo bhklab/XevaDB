c56611c1b1a74b88587bd5baebe70f996699d954
gangeshberi <gangeshberi@gmail.com>
2021-05-18 15:31:12 -0400

    Add: new component `tooltip.js`.

---
+++ client/src/Components/Utils/ToolTip.js
@@ -0,0 +1,17 @@
+import { select } from 'd3';
+
+const createToolTip = (id) => {
+    select(`#${id}`)
+        .append('div')
+        .attr('id', 'tooltip')
+        .style('position', 'absolute')
+        .style('visibility', 'hidden')
+        .style('border', 'solid')
+        .style('border-width', '1px')
+        .style('border-radius', '5px')
+        .style('padding', '5px')
+        .attr('top', 10)
+        .attr('left', 10);
+};
+
+export default createToolTip;
