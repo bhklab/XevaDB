ff027ba9af645ee9adf85bf433dbfc404f1c1a44
gangeshberi <gangeshberi@gmail.com>
2021-05-19 02:55:05 -0400

    Add: a new function to transform  the data for the barplot.

--- client/src/Components/SummaryPages/PatientSummary/PatientSummary.js
+++ client/src/Components/SummaryPages/PatientSummary/PatientSummary.js
@@ -4,39 +4,54 @@ import Spinner from '../../Utils/Spinner';
 import GlobalStyles from '../../../GlobalStyles';
 import Footer from '../../Footer/Footer';
 import PatientTable from './PatientTable';
+import BarPlot from '../../Plots/BarPlot';
 
+// transform the data for the patient table.
+const transformData = (data) => {
+    const transformedData = {};
+
+    data.forEach((row) => {
+        const patient = transformedData[row.patient];
+
+        if (patient && !patient.models.includes(row.model)) {
+            transformedData[row.patient].model_count += 1;
+            patient.models.push(row.model);
+        }
+        if (patient && !patient.drugs.includes(row.drug)) {
+            transformedData[row.patient].drug_count += 1;
+            patient.drugs.push(row.drug_name);
+        } else {
+            transformedData[row.patient] = {
+                dataset_id: row.dataset_id,
+                dataset: row.dataset_name,
+                patient: row.patient,
+                patient_id: row.patient_id,
+                model_count: 1,
+                drug_count: 1,
+                models: [row.model],
+                drugs: [row.drug_name],
+            };
+        }
+    });
+    console.log(transformedData);
+    return transformedData;
+};
+
+// transform data for BarPlot.
+const barPlotData = (data) => {
+    const barData = data.map((element) => (
+        {
+            id: element.patient,
+            value: element.drugs.length,
+        }
+    ));
+    return barData;
+};
+
+// patient Summary Component
 const PatientSummary = () => {
     const [patientData, setPatientDataState] = useState([]);
     const [loading, setLoader] = useState(true);
-
-    const transformData = (data) => {
-        const transformedData = {};
-
-        data.forEach((row) => {
-            const patient = transformedData[row.patient];
-
-            if (patient && !patient.models.includes(row.model)) {
-                transformedData[row.patient].model_count += 1;
-                patient.models.push(row.model);
-            }
-            if (patient && !patient.drugs.includes(row.drug)) {
-                transformedData[row.patient].drug_count += 1;
-                patient.drugs.push(row.drug_name);
-            } else {
-                transformedData[row.patient] = {
-                    dataset_id: row.dataset_id,
-                    dataset: row.dataset_name,
-                    patient: row.patient,
-                    patient_id: row.patient_id,
-                    model_count: 1,
-                    drug_count: 1,
-                    models: [row.model],
-                    drugs: [row.drug_name],
-                };
-            }
-        });
-        return transformedData;
-    };
 
     const fetchData = async () => {
         // api request to get the required data.
@@ -54,6 +69,17 @@ const PatientSummary = () => {
         <>
             <GlobalStyles />
             <div className="wrapper">
+                <div className="donut-wrapper">
+                    <h1> Number of Drugs Tested on a Patient </h1>
+                    {
+                        loading ? <Spinner loading={loading} />
+                            : (
+                                <BarPlot
+                                    data={barPlotData(patientData.slice(1, 50))}
+                                />
+                            )
+                    }
+                </div>
                 <div className="summary-table" style={{ marginTop: '4vh' }}>
                     {
                         loading ? (<Spinner loading={loading} />)
