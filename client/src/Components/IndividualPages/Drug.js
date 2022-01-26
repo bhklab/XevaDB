b1d93e3c9274ddb1dba7737b6f5d0bbf3eb3d954
Gangesh Beri <41119826+gangeshberi@users.noreply.github.com>
2022-01-25 22:32:37 -0500

    Update: `drug individual` page component

--- client/src/Components/IndividualPages/Drug.js
+++ client/src/Components/IndividualPages/Drug.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Footer from '../Footer/Footer';
+import GlobalStyles from '../../GlobalStyles';
+import Spinner from '../Utils/Spinner';
 
-const Drug = () => {
+// header constant.
+const HEADER = { headers: { Authorization: localStorage.getItem('user') } };
+
+const Drug = (props) => {
+    // get the drug id from the props object 
+    // and assign it to drugId variable
+    const { match: { params: { id: drugId } } } = props;
+
+    // state to save the drug information data and setting loader state
+    const [drugData, setDrugData] = useState({});
+    const [isLoading, setLoadingState] = useState(true);
+
+
+    // query to fetch the drug information data
+    const fetchData = async () => {
+        // get the drug information based on the drugId
+        const drugInformation = await axios.get(`/api/v1/drugs/${drugId}`, HEADER);
+        console.log(drugInformation);
+
+        // update the state of data
+        const { data } = drugInformation;
+        setDrugData(data[0]);
+
+        // set loader state
+        setLoadingState(false);
+    };
+
+    // useEffect hook
+    useEffect(() => {
+        // fetch the data
+        fetchData();
+    }, []);
+
     return (
-        <h1 style={{ margin: '200px' }}> Individual Drug page!</h1>
+        <>
+            <GlobalStyles />
+            <div className='wrapper'>
+                {
+                    isLoading
+                        ? <Spinner loading={isLoading} />
+                        : (
+                            <h1> {drugData.drug_name} </h1>
+                        )
+                }
+            </div>
+            <Footer />
+        </>
     )
 };
 
