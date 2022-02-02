11ef89db378a55a2eac0f62743f94dbc167c94e3
Gangesh Beri <41119826+gangeshberi@users.noreply.github.com>
2022-02-01 19:56:33 -0500

    Add: new styled component to the `Biomarker` page

--- client/src/Components/Biomarker/Biomarker.js
+++ client/src/Components/Biomarker/Biomarker.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import GlobalStyles from '../../GlobalStyles';
+import Footer from '../Footer/Footer';
 import ForestPlot from '../Plots/ForestPlot';
+import styled from 'styled-components';
 import data from './data';
 
+// Biomarker styles
+const StyledBiomarker = styled.div`
+    h1 {
+        margin: 5px 0 60px 0;
+    }
+`;
 
+
+// Biomarker component
 const Biomarker = () => {
     return (
-        <>
+        <StyledBiomarker>
             <GlobalStyles />
             <div className='wrapper'>
                 <div className="donut-wrapper">
@@ -14,7 +24,8 @@ const Biomarker = () => {
                     <ForestPlot data={data.gctd} />
                 </div>
             </div>
-        </>
+            <Footer />
+        </StyledBiomarker>
     )
 };
 
