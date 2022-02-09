8481c58eb2aa4c9d3ed186eef018c71d6b8e1cc4
Gangesh Beri <41119826+gangeshberi@users.noreply.github.com>
2022-02-09 14:06:28 -0500

    Add: functions to make the API call and get the data from the backend

--- client/src/Components/Biomarker/BiomarkerSelect.js
+++ client/src/Components/Biomarker/BiomarkerSelect.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Select from 'react-select';
 import colors from '../../styles/colors';
 import { customStyles } from '../Search/SearchStyle';
@@ -9,7 +10,7 @@ const StyledSelect = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-    margin: 2% 5% 5% 10%;
+    margin: 2% 5% 7.5% 10%;
     
     .drug-select, .gene-select {
         width: 30%;
@@ -25,17 +26,59 @@ const StyledSelect = styled.div`
     }
 `;
 
-const BiomarkerSelect = () => (
-    <StyledSelect className='biomarker-select'>
-        <div className='drug-select'>
-            <span> Select Drug </span>
-            <Select styles={customStyles} />
-        </div>
-        <div className='gene-select'>
-            <span> Select Gene </span>
-            <Select styles={customStyles} />
-        </div>
-    </StyledSelect>
-);
+const BiomarkerSelect = () => {
+    const [drugs, setDrugs] = useState([]);
+    const [genes, setGenes] = useState([]);
+
+    // function to get the drug data
+    const getDrugs = async function () {
+        // API call to get the list of drugs
+        const drugResponse = await axios.get('/api/v1/drugs', { headers: { Authorization: localStorage.getItem('user') } });
+
+        // prepare data for drug selection
+        const drugSelectionData = drugResponse.data.map(el => ({
+            value: el.drug_id,
+            label: el.drug_name,
+        }));
+
+        // setting the state
+        setDrugs(drugSelectionData);
+    };
+
+    // function to get the gene data
+    const getGenes = async function () {
+        // API call to get the list of genes
+        const geneResponse = await axios.get('/api/v1/genes', { headers: { Authorization: localStorage.getItem('user') } });
+
+        // prepare data for gene selection
+        const geneSelectionData = geneResponse.data.data.map(el => ({
+            value: el.gene_id,
+            label: el.gene_name,
+        }));
+
+        // setting the gene state
+        setGenes(geneSelectionData);
+    };
+
+    useEffect(() => {
+        // calling getDrugs function
+        getDrugs();
+        // calling getGenes function
+        getGenes();
+    }, []);
+
+    return (
+        < StyledSelect className='biomarker-select' >
+            <div className='drug-select'>
+                <span> Select Drug </span>
+                <Select styles={customStyles} options={drugs} />
+            </div>
+            <div className='gene-select'>
+                <span> Select Gene </span>
+                <Select styles={customStyles} options={genes} />
+            </div>
+        </StyledSelect >
+    )
+};
 
 export default BiomarkerSelect;
