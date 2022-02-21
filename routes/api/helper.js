cd9c21fdacc25831c64a2aa7bd400785dbde9755
gangesh beri <gangeshberi@gmail.com>
2022-02-20 22:26:12 -0500

    Add: new `drugsBasedOnDatasetIdQuery` and `patientsBasedOnDatasetIdQuery` functions to helper file

--- routes/api/helper.js
+++ routes/api/helper.js
@@ -30,15 +30,39 @@ const geneListQuery = (genes) => knex.select('gene_id')
  */
 const distinctDrugsQuery = (dataset) => (
     knex.distinct('drug_id')
-        .from('model_information')
+        .from('datasets_drugs')
         .where({
             dataset_id: dataset,
         })
 );
 
 
+/**
+ * @param {number} dataset - dataset id.
+ * @returns {Array} - an array of the distinct drug ids and drug names based on the dataset id.
+ */
+const drugsBasedOnDatasetIdQuery = (dataset) => knex
+    .distinct('drugs.drug_name as drug', 'drugs.drug_id')
+    .from('datasets_drugs')
+    .join('drugs', 'drugs.drug_id', 'datasets_drugs.drug_id')
+    .where({ dataset_id: dataset });
+
+
+/**
+ * @param {number} dataset- dataset id to get the patient from particular dataset.
+ * @returns {Object} - knex query to grab an array of distinct patients
+ * for a particular dataset.
+ */
+const patientsBasedOnDatasetIdQuery = (dataset) => knex
+    .distinct('patients.patient', 'patients.patient_id')
+    .from('patients')
+    .where({ dataset_id: dataset });
+
+
 module.exports = {
     distinctPatientsQuery,
     geneListQuery,
     distinctDrugsQuery,
+    drugsBasedOnDatasetIdQuery,
+    patientsBasedOnDatasetIdQuery,
 };
