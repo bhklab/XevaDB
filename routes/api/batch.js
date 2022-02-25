98bcba35e936c2b06fc5120bcf28261b3a98688a
gangesh beri <gangeshberi@gmail.com>
2022-02-25 15:31:29 -0500

    Add: new function to get `batch id` added to `batch` API

--- routes/api/batch.js
+++ routes/api/batch.js
@@ -2,6 +2,31 @@
 const knex = require('../../db/knex1');
 
 
+// ************************************** Batch Queries ***************************************************
+/**
+ * @returns {Object} - a query to get the batch id
+ */
+const batchIdQuery = () => knex
+    .select('batch_information.batch_id', 'model_information.dataset_id')
+    .from('batch_information')
+    .join(
+        'model_information',
+        'batch_information.model_id',
+        'model_information.model_id',
+    )
+    .join(
+        'patients',
+        'model_information.patient_id',
+        'patients.patient_id',
+    )
+    .join(
+        'drugs',
+        'model_information.drug_id',
+        'drugs.drug_id',
+    );
+
+
+// ************************************** API Endpoints Functions ***************************************************
 /**
  * @param {Object} request - request object.
  * @param {Object} response - response object with authorization header.
@@ -22,5 +47,6 @@ const getAllBatches = (request, response) => {
 
 
 module.exports = {
+    batchIdQuery,
     getAllBatches,
 };
