1ebc68216d828e69022aff5c7285a5815bbef83e
gangesh beri <gangeshberi@gmail.com>
2022-03-01 18:45:43 -0500

    Add: new util function `mRECISTColorMapper`

---
+++ client/src/utils/mRECISTColorMapper.js
@@ -0,0 +1,12 @@
+import colors from '../styles/colors';
+
+// maps mRECIST response to values from 1 - 4
+const mRECISTColorMapper = {
+    'PD': `${colors.red}`,
+    'SD': `${colors.yellow}`,
+    'PR': `${colors.green}`,
+    'CR': `${colors.blue}`,
+    'NA': 0,
+};
+
+export default mRECISTColorMapper;
