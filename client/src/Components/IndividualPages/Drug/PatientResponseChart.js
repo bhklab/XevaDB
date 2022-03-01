a59b28f86ff1dbd3c48053b008eabf43400d60f1
gangesh beri <gangeshberi@gmail.com>
2022-03-01 12:32:00 -0500

    Update: `PatientResponseChart` to add new functions

--- client/src/Components/IndividualPages/Drug/PatientResponseChart.js
+++ client/src/Components/IndividualPages/Drug/PatientResponseChart.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import BarPlot from '../../Plots/BarPlot';
+import mRECISTMapper from '../../../utils/mRECISTMapper';
+import Spinner from '../../Utils/Spinner';
 
 // header constant
 const HEADER = { headers: { Authorization: localStorage.getItem('user') } };
@@ -8,21 +11,58 @@ const HEADER = { headers: { Authorization: localStorage.getItem('user') } };
 const PatientResponseChart = () => {
     // model response data
     const [modelResponseData, setModelResponseData] = useState([]);
+    const [isLoading, setLoadingState] = useState(true);
+
+    // transform model response data
+    const transformModelResponseData = (data) => {
+        // transformed object
+        const transformedArray = [];
+
+        // iterate through data and add an object to transformed Array
+        Object.keys(data).forEach(element => {
+            if (element !== 'Drug') {
+                transformedArray.push({
+                    id: element,
+                    value: mRECISTMapper[data[element].mRECIST],
+                })
+            };
+        });
+
+        return transformedArray;
+    };
+
+    // fetch model response data
+    const fetchData = async () => {
+        // fetch model response data
+        const modelResponse = await axios.get(`/api/v1/modelresponse?drug=BGJ398`, HEADER);
+
+        // transform model response data
+        const transformedModelResponse = transformModelResponseData(modelResponse.data[0]);
+
+        // set the model response data
+        setModelResponseData(transformedModelResponse);
+
+        // update the loading state
+        setLoadingState(false);
+    };
 
     // use effect react hook
     useEffect(() => {
         // fetch data function
-        fetchData();
-    });
+        const modelResponse = fetchData();
 
-    // fetch model response data
-    const fetchData = async () => {
-        const modelResponse = await axios.get(`/api/v1/modelresponse?drug=BGJ398`, HEADER);
-        console.log(modelResponse);
-    };
+    }, []);
 
     return (
-        <h1> Patient Response </h1>
+        isLoading ? <Spinner loading={isLoading} /> : (
+            <>
+                <h1> Model Response </h1>
+                <BarPlot
+                    data={modelResponseData}
+                    label="Model Response"
+                />
+            </>
+        )
     )
 };
 
