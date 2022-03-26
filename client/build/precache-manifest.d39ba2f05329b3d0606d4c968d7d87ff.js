8d842cae02f1cca96812ab420e0af2a693402fbf
gangesh beri <gangeshberi@gmail.com>
2022-03-26 14:28:07 -0400

    new build march-2022

---
+++ client/build/precache-manifest.d39ba2f05329b3d0606d4c968d7d87ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b49bb9a20e5e48674c68ab79962271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65d69962445ffbdff8d4",
+    "url": "/static/css/2.9f42b52c.chunk.css"
+  },
+  {
+    "revision": "65d69962445ffbdff8d4",
+    "url": "/static/js/2.de3de295.chunk.js"
+  },
+  {
+    "revision": "0886b67d223513a9e123f45ddf598488",
+    "url": "/static/js/2.de3de295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c107dcd33f090f4201d",
+    "url": "/static/js/main.a8feafc9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "f678bfa501f4bec32eb33380bb861ed4",
+    "url": "/static/media/download.f678bfa5.svg"
+  },
+  {
+    "revision": "2135ae7da234f96ba09933f67ee3ea9a",
+    "url": "/static/media/human.2135ae7d.png"
+  },
+  {
+    "revision": "703c5c4552fdaa31c24f8de36dc468f9",
+    "url": "/static/media/largeintestine.703c5c45.png"
+  },
+  {
+    "revision": "34f7ebc872e8a46ec26af1dcde7923e2",
+    "url": "/static/media/lung.34f7ebc8.png"
+  },
+  {
+    "revision": "b694f1dd13bab7f6947efa97502a62f0",
+    "url": "/static/media/new_logo.b694f1dd.png"
+  },
+  {
+    "revision": "c2f8962dbe29e336342528f9d776beec",
+    "url": "/static/media/skin.c2f8962d.png"
+  }
+]);
