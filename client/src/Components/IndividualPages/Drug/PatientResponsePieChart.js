67eff8047397604f3882bd4f2a53600e2974b3ec
gangesh beri <gangeshberi@gmail.com>
2022-03-30 16:06:47 -0400

    Update: `PatientResponsePieChart` component to add new transform function

--- client/src/Components/IndividualPages/Drug/PatientResponsePieChart.js
+++ client/src/Components/IndividualPages/Drug/PatientResponsePieChart.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DonutChart from '../../Plots/DonutChart';
 
-const PatientResponsePieChart = () => {
+/**
+ * 
+ * @param {Object} data 
+ * @returns {Array} - an array of the transformed data
+ */
+const transformData = (data) => {
+    // stores the final data
+    let finalData = {};
+
+    // loops through data and prepare data based on the 'mRECIST' value
+    Object.values(data).forEach(element => {
+        if (typeof (element) === 'object') {
+            element.mRECIST.forEach(value => {
+                if (finalData.hasOwnProperty(value)) {
+                    finalData[value].value += 1;
+                } else {
+                    finalData[value] = {
+                        id: value,
+                        parameter: 1,
+                        value: 1,
+                    }
+                }
+            })
+        }
+    });
+
+    return Object.values(finalData);
+};
+
+
+
+const PatientResponsePieChart = ({ data }) => {
+
+    // set the data states
+    const [responseData, setResponseData] = useState([]);
+
+    useEffect(() => {
+        // get the transformed data
+        const transformedData = transformData(data);
+        // set data state
+        setResponseData(transformedData);
+    }, [data]);
+
     return (
-        <h1> Patient Response </h1>
+        responseData.length === 0 ? '' :
+            <DonutChart
+                data={responseData}
+                chartId='patient-response-donut'
+            />
     );
 };
 
