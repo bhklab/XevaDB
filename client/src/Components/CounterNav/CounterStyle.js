b6ff4129d08915eafda9bcc2c8d2445f4c8ab916
gangesh beri <gangeshberi@gmail.com>
2022-06-22 17:30:01 -0400

    Update: styles for footer, counter and navigation

--- client/src/Components/CounterNav/CounterStyle.js
+++ client/src/Components/CounterNav/CounterStyle.js
@@ -4,8 +4,8 @@ import colors from '../../styles/colors';
 const CounterStyle = styled.div`
     display: flex;
     background-color: ${colors.white};
-    min-height: 6.5vh;
-    max-height: 6.5vh;
+    min-height: 7.5vh;
+    max-height: 7.5vh;
     position: fixed;
     bottom: 0;
     left: 0;
