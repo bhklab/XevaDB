a1635b5ca01449a8e74fff1248f5a60ffc97a80e
gangesh beri <gangeshberi@gmail.com>
2022-07-14 18:50:57 -0400

    Update: `ExportPng` component updated styles

--- client/src/Components/Utils/ExportPng.js
+++ client/src/Components/Utils/ExportPng.js
@@ -5,24 +5,22 @@ import colors from '../../styles/colors';
 import downloadIcon from '../../images/download.svg';
 
 const StyledButton = styled.div`
-    font-weight: 500;
     display: inline !important;
     // align-self: flex-end;
     margin-top: 40px;
     
     button {
+        font-weight: 300;
         background-color: ${colors['--bg-color']} !important;
         color: ${colors['--white']} !important;
-        padding: 8px !important;
+        padding: 10px 2px !important;
         margin-right: 5px;
         border-radius: 6px;
-        border: 1px;
-        font-size: 1.05em;
-        width: 170px;
+        border: none;
+        font-size: 1em;
+        width: 150px;
         :hover {
-            border: 1px solid ${colors['--bg-color']};
-            color: ${colors['--bg-color']} !important;
-            background-color: ${colors['--white']} !important;
+            opacity: 0.75;
             cursor: pointer;
         }
     }
