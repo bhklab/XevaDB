17e117f4d883074401d304f17c4660f3ccc3610b
void-work <void-work@void-works-MacBook-Pro.local>
2022-09-08 13:47:10 -0400

    update to  for the client

--- client/src/keycloak.js
+++ client/src/keycloak.js
@@ -1,11 +1,11 @@
-import Keycloak from 'keycloak-js'
+import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = new Keycloak({
-    realm: "xevadb",
-    url: "http://localhost:8080/",
-    clientId: 'xevadb',
+    realm: 'xevadb-realm',
+    url: 'http://localhost:8080/',
+    clientId: 'xevadb-client',
 });
 
 export default keycloak;
