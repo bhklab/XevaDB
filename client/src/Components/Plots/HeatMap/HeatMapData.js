a3706db298bc5fb9c1593a9a55214a016a886801
gangeshberi <gangeshberi@gmail.com>
2023-01-22 23:10:20 -0500

    Update: HeatMapData component modified to react hooks.

--- client/src/Components/Plots/HeatMap/HeatMapData.js
+++ client/src/Components/Plots/HeatMap/HeatMapData.js
@@ -1,174 +1,172 @@
-/* eslint-disable no-shadow */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import Spinner from '../../Utils/Spinner';
 import HeatMap from './HeatMap';
 import { OncoprintGenes } from '../../../utils/OncoprintGenes';
 
-class HeatMapData extends React.Component {
-    constructor(props) {
-        super(props);
-        // setting the states for the data.
-        this.state = {
-            data: [],
-            patientId: [],
-            drugId: [],
-            datasetIdProp: 0,
-            drugListProp: '',
-            geneListProp: '',
-            dimensions: { height: 30, width: 14 },
-            margin: {
-                top: 200, right: 250, bottom: 50, left: 250,
-            },
-            loading: true,
-        };
-    }
+// dimension and margin variables
+const dimensions = { height: 30, width: 14 };
+const margin = {
+    top: 200, right: 250, bottom: 50, left: 250,
+};
 
-    static getDerivedStateFromProps(props) {
-        const { datasetId, geneList, drugList } = props;
-        return {
-            datasetIdProp: datasetId,
-            geneListProp: geneList ? geneList.split(',') : OncoprintGenes,
-            drugListProp: drugList,
-        };
-    }
+// fetch the patient list and model response data
+const fetchData = async (drugList, datasetId) => {
+    // get model response data and list of patients based on the dataset id
+    let modelResponse;
 
-    componentDidMount() {
-        const { datasetIdProp } = this.state;
-        if (datasetIdProp > 0) {
-            this.fetchData();
-        }
-    }
-
-    fetchData = async () => {
-        // get model response data and list of patients based on the dataset id
-        let modelResponse = '';
-        const { drugListProp, datasetIdProp } = this.state;
-
-        if (drugListProp) {
-            modelResponse = axios.get(`/api/v1/modelresponse?drug=${drugListProp}&dataset=${datasetIdProp}`, { headers: { Authorization: localStorage.getItem('user') } });
-        } else {
-            modelResponse = axios.get(`/api/v1/modelresponse/${datasetIdProp}`, { headers: { Authorization: localStorage.getItem('user') } });
-        }
-
-        // patient API call
-        const patients = axios.get(`/api/v1/datasets/detail/${datasetIdProp}`, { headers: { Authorization: localStorage.getItem('user') } });
-
-        // running both API calls in parallel
-        const data = await Promise.all([modelResponse, patients]);
-
-        // get model response and patient data
-        const modelResponseData = data[0].data;
-        const patientsData = data[1].data.datasets[0].patients;
-
-        this.parseData(modelResponseData, patientsData);
-    }
-
-    // this function takes the parsed result and set the states.
-    parseData = (modelResponse, patients) => {
-        const dataset = [];
-        let patientArray = patients;
-        const drug = [];
-
-        // this function will loop through the elements and
-        // assign empty values in case model information is not available for the patient.
-        modelResponse.forEach((element) => {
-            const dataObject = {};
-            drug.push(element.Drug);
-            patientArray.forEach((patient) => {
-                if (!element[patient]) {
-                    dataObject[patient] = '';
-                } else {
-                    dataObject[patient] = element[patient];
-                }
-            });
-            dataset.push(dataObject);
-        });
-
-        // patient from one of the object elements to keep it in sync.
-        patientArray = Object.keys(dataset[0]).sort();
-
-        // TODO: Update the 'data' for now; change later when we have average data
-        // TODO: 'mRECIST' will be the max occuring value and the other parameters are just taking the first element
-        // finds the maximum occurences of a 'mRECIST' type
-        const maxOccuringmRECISTValue = (mRECIST) => {
-            // this object will store the occurences of mRECIST values
-            const mRECISTObject = {};
-            mRECIST.forEach((el) => {
-                if (mRECISTObject.hasOwnProperty(el)) {
-                    mRECISTObject[el] += 1;
-                } else {
-                    mRECISTObject[el] = 1;
-                }
-            });
-
-            // gets the maximum occuring mRECIST value
-            let maxOccuringKey = '';
-            let maxValue = 0;
-            Object.entries(mRECISTObject).forEach(([key, value]) => {
-                if (value > maxValue) {
-                    maxValue = value;
-                    maxOccuringKey = key;
-                }
-            });
-
-            return maxOccuringKey;
-        };
-
-        // transforms the 'dataset'
-        const transformedData = dataset.map((row) => {
-            const transformedRow = {};
-            Object.entries(row).forEach(([key, value]) => {
-                transformedRow[key] = {
-                    'best.average.response': value['best.average.response']?.[0] || 'NA',
-                    survival: value.survival?.[0] || 'NA',
-                    slope: value.slope?.[0] || 'NA',
-                    AUC: value.AUC?.[0] || 'NA',
-                    mRECIST: value.mRECIST ? maxOccuringmRECISTValue(value.mRECIST) : 'NA',
-                };
-            });
-            return transformedRow;
-        });
-
-        // setting state
-        this.setState({
-            drugId: drug,
-            patientId: patientArray,
-            // data: dataset,
-            data: transformedData,
-            loading: false,
-        });
-    }
-
-    render() {
-        const {
-            data, drugId, loading, patientId, dimensions,
-            margin, datasetIdProp, geneListProp,
-        } = this.state;
-
-        return (
-            <div>
-                {
-                    loading
-                        ? <Spinner loading={loading} />
-                        : (
-                            <HeatMap
-                                data={data}
-                                drugId={drugId}
-                                patientId={patientId}
-                                dimensions={dimensions}
-                                margin={margin}
-                                dataset={datasetIdProp}
-                                className='heatmap'
-                                geneList={geneListProp}
-                            />
-                        )
-                }
-            </div>
+    if (drugList) {
+        modelResponse = axios.get(
+            `/api/v1/modelresponse?drug=${drugList}&dataset=${datasetId}`,
+            { headers: { Authorization: localStorage.getItem('user') } },
+        );
+    } else {
+        modelResponse = axios.get(
+            `/api/v1/modelresponse/${datasetId}`,
+            { headers: { Authorization: localStorage.getItem('user') } },
         );
     }
-}
+
+    // patient API call
+    const patients = axios.get(
+        `/api/v1/datasets/detail/${datasetId}`,
+        { headers: { Authorization: localStorage.getItem('user') } },
+    );
+
+    // running both API calls in parallel
+    const data = await Promise.all([modelResponse, patients]);
+
+    // get model response and patient data
+    const modelResponseData = data[0].data;
+    const patientList = data[1].data.datasets[0].patients;
+
+    return [modelResponseData, patientList];
+};
+
+// this function takes the parsed result and set the states.
+const parseData = (modelResponse, patients) => {
+    const dataset = [];
+    let patientArray = patients;
+    const drug = [];
+
+    // this function will loop through the elements and
+    // assign empty values in case model information is not available for the patient.
+    modelResponse.forEach((element) => {
+        const dataObject = {};
+        drug.push(element.Drug);
+        patientArray.forEach((patient) => {
+            if (!element[patient]) {
+                dataObject[patient] = '';
+            } else {
+                dataObject[patient] = element[patient];
+            }
+        });
+        dataset.push(dataObject);
+    });
+
+    // patient from one of the object elements to keep it in sync.
+    patientArray = Object.keys(dataset[0]).sort();
+
+    // TODO: Update the 'data' for now; change later when we have average data
+    // TODO: 'mRECIST' will be the max occuring value and the other parameters are just taking the first element
+    // finds the maximum occurences of a 'mRECIST' type
+    const maxOccuringmRECISTValue = (mRECIST) => {
+        // this object will store the occurences of mRECIST values
+        const mRECISTObject = {};
+        mRECIST.forEach((el) => {
+            if (mRECISTObject.hasOwnProperty(el)) {
+                mRECISTObject[el] += 1;
+            } else {
+                mRECISTObject[el] = 1;
+            }
+        });
+
+        // gets the maximum occuring mRECIST value
+        let maxOccuringKey = '';
+        let maxValue = 0;
+        Object.entries(mRECISTObject).forEach(([key, value]) => {
+            if (value > maxValue) {
+                maxValue = value;
+                maxOccuringKey = key;
+            }
+        });
+
+        return maxOccuringKey;
+    };
+
+    // transforms the 'dataset'
+    const transformedData = dataset.map((row) => {
+        const transformedRow = {};
+        Object.entries(row).forEach(([key, value]) => {
+            transformedRow[key] = {
+                'best.average.response': value['best.average.response']?.[0] || 'NA',
+                survival: value.survival?.[0] || 'NA',
+                slope: value.slope?.[0] || 'NA',
+                AUC: value.AUC?.[0] || 'NA',
+                mRECIST: value.mRECIST ? maxOccuringmRECISTValue(value.mRECIST) : 'NA',
+            };
+        });
+        return transformedRow;
+    });
+
+    return {
+        drugList: drug,
+        patientList: patientArray,
+        responseData: transformedData,
+    };
+};
+
+/**
+ * main component
+ */
+const HeatMapData = (props) => {
+    const { datasetId, drugList: drugProp, geneList: geneProp } = props;
+    const geneList = geneProp ? geneProp.split(',') : OncoprintGenes;
+    const [responseData, setResponseData] = useState([]);
+    const [patientList, setPatientList] = useState([]);
+    const [drugList, setDrugList] = useState([]);
+    const [isLoading, setLoadingState] = useState(true);
+
+    useEffect(() => {
+        console.log('helllla');
+        if (datasetId > 0) {
+            fetchData(drugProp, datasetId)
+                .then((data) => {
+                    const [responseArray, patientArray] = data;
+                    return parseData(responseArray, patientArray);
+                })
+                .then((data) => {
+                    console.log(data);
+                    setResponseData(data.responseData);
+                    setPatientList(data.patientList);
+                    setDrugList(data.drugList);
+                    setLoadingState(false);
+                });
+        }
+    }, [props]);
+
+    return (
+        <div>
+            {
+                isLoading
+                    ? <Spinner loading={isLoading} />
+                    : (
+                        <HeatMap
+                            data={responseData}
+                            drugId={drugList}
+                            patientId={patientList}
+                            dimensions={dimensions}
+                            geneList={geneList}
+                            margin={margin}
+                            dataset={datasetId}
+                            className='heatmap'
+                        />
+                    )
+            }
+        </div>
+    );
+};
 
 HeatMapData.propTypes = {
     datasetId: PropTypes.oneOfType([
