3111a1489acd39cee268299cb2d3ec4a24dd0b29
gangeshberi <gangeshberi@gmail.com>
2023-02-15 20:54:55 -0500

    Updated `getAllGenes` to add a new function to remove the unwanted genes.

--- routes/api/gene.js
+++ routes/api/gene.js
@@ -1,16 +1,26 @@
 /* eslint-disable func-names */
 const knex = require('../../db/knex1');
 
+/**
+ *
+ * @param genes {Array} - array of gene object
+ * @returns {Array} - removing the data time string and returning a filtered array
+ */
+const removeUnwantedGenes = (genes) => {
+    // regex is matching a string like '2022-03-01 00:00:00'
+    const regex = new RegExp(/[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}/, 'g');
 
-// ************************************** Gene Queries ***************************************************
+    return genes.filter((geneObject) => !geneObject.gene_name.match(regex));
+};
+
+// ************************************** Gene Queries ******************************************
 /**
  * @returns {Object} - returns a Knex query to select all the genes
  */
 const getAllGenesQuery = () => knex.select()
     .from('genes');
 
-
-// ************************************** API Endpoint Functions ******************************************
+// ************************************** API Endpoint Functions *********************************
 /**
  * @param {Object} request - request object.
  * @param {Object} response - response object with authorization header.
@@ -18,6 +28,7 @@ const getAllGenesQuery = () => knex.select()
  */
 const getAllGenes = (request, response) => {
     getAllGenesQuery()
+        .then((genes) => removeUnwantedGenes(genes))
         .then((genes) => response.status(200).json({
             status: 'success',
             data: genes,
@@ -27,7 +38,6 @@ const getAllGenes = (request, response) => {
             data: error,
         }));
 };
-
 
 module.exports = {
     getAllGenes,
