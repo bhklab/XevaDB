6d6e18f18d188f21e7f28e26da737a4ae0f814a9
gangeshberi <gangeshberi@gmail.com>
2023-02-24 18:46:16 -0500

    Fix: `TableFiltering` util function to add type conversion to fix a minor issue.

--- client/src/Components/Utils/TableFiltering.js
+++ client/src/Components/Utils/TableFiltering.js
@@ -13,11 +13,11 @@ const tableFilter = (filter, row) => {
     // regex to match the search value in the string
     // and return true if it matches
     const id = filter.pivotId || filter.id;
-    const regex = new RegExp(filter.value, 'gi');
+    const regex = new RegExp(String(filter.value), 'gi');
 
     const match = checkIfAnObject(row[id])
         ? row[id]?.name.match(regex)
-        : row[id]?.match(regex);
+        : String(row[id])?.match(regex);
 
     return !!match;
 };
